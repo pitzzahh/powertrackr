cb4359d253f711bf62b1c8b5a766d351931285e5
Peter John Arao <pitzzahh@gmail.com>
2024-01-17 21:15:22 +0800

    ♻️ Refactor data-table-actions.svelte component

--- src/routes/history/data-table-actions.svelte
+++ src/routes/history/data-table-actions.svelte
@@ -1,30 +1,39 @@
 <script lang="ts">
-    import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
-    import { Button } from "$lib/components/ui/button";
-    import { MoreHorizontal } from "lucide-svelte";
-    export let id: string;
-  </script>
-  <DropdownMenu.Root>
-    <DropdownMenu.Trigger asChild let:builder>
-      <Button
-        variant="ghost"
-        builders={[builder]}
-        size="icon"
-        class="relative w-8 h-8 p-0"
-      >
-        <span class="sr-only">Open menu</span>
-        <MoreHorizontal class="w-4 h-4" />
-      </Button>
-    </DropdownMenu.Trigger>
-    <DropdownMenu.Content>
-      <DropdownMenu.Group>
-        <DropdownMenu.Label>Actions</DropdownMenu.Label>
-        <DropdownMenu.Item on:click={() => navigator.clipboard.writeText(id)}>
-          Copy payment ID
-        </DropdownMenu.Item>
-      </DropdownMenu.Group>
-      <DropdownMenu.Separator />
-      <DropdownMenu.Item>View customer</DropdownMenu.Item>
-      <DropdownMenu.Item>View payment details</DropdownMenu.Item>
-    </DropdownMenu.Content>
-  </DropdownMenu.Root>
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { Button } from '$lib/components/ui/button';
+	import { MoreHorizontal } from 'lucide-svelte';
+	import { toast } from 'svelte-sonner';
+	import { formatDate } from '$lib';
+	export let id: string;
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button variant="ghost" builders={[builder]} size="icon" class="relative h-8 w-8 p-0">
+			<span class="sr-only">Open menu</span>
+			<MoreHorizontal class="h-4 w-4" />
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		<DropdownMenu.Group>
+			<DropdownMenu.Label>Actions</DropdownMenu.Label>
+			<DropdownMenu.Item
+				on:click={() => {
+					toast.success('Payment ID copied to clipboard', {
+						description: formatDate(new Date()),
+						action: {
+							label: 'Undo',
+							onClick: () => console.log('Undo')
+						}
+					});
+					navigator.clipboard.writeText(id);
+				}}
+			>
+				Copy payment ID
+			</DropdownMenu.Item>
+		</DropdownMenu.Group>
+		<DropdownMenu.Separator />
+		<DropdownMenu.Item>View customer</DropdownMenu.Item>
+		<DropdownMenu.Item>View payment details</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
