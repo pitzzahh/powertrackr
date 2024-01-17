773455ce0fa035744e99c7bd7c725ccdc8bd6d67
Peter John Arao <pitzzahh@gmail.com>
2024-01-17 19:58:29 +0800

    ✨ Add dev mode injection and analytics import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '$lib/components/header.svelte';
 	import { onNavigate } from '$app/navigation';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 
 	onNavigate((navigation) => {
 		// @ts-ignore
@@ -18,6 +20,7 @@
 		});
 	});
 
+	inject({ mode: dev ? 'development' : 'production' });
 	injectSpeedInsights();
 </script>
 
