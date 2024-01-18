7469afc2d8eee4d6623eb616de5bc1ae1aef5012
Peter John Arao <pitzzahh@gmail.com>
2024-01-18 11:19:01 +0800

    🔧 Add auth middleware to lucia server

---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,11 @@
+import { lucia } from "lucia";
+import { sveltekit } from "lucia/middleware";
+import { dev } from "$app/environment";
+import { prisma } from "@lucia-auth/adapter-prisma";
+import prismaClient  from '$lib/server/prisma'
+
+export const auth = lucia({
+    env: dev ? "DEV" : "PROD",
+    middleware: sveltekit(),
+    adapter: prisma(prismaClient),
+});
