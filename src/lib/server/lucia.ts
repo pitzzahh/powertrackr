9fe271457ec95a0d3313fdf63455dde0a5cddb0f
Peter John Arao <pitzzahh@gmail.com>
2024-01-19 00:21:58 +0800

    ♻️ Update PrismaClient import in lucia.ts

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -2,7 +2,7 @@ import { lucia } from "lucia";
 import { sveltekit } from "lucia/middleware";
 import { dev } from "$app/environment";
 import { prisma } from "@lucia-auth/adapter-prisma";
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from '@prisma/client'
 
 export const auth = lucia({
     env: dev ? "DEV" : "PROD",
