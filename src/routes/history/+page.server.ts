1fcbcbd2b0bf1213ef91a9910561a43c32dadae9
Peter John Arao <pitzzahh@gmail.com>
2024-01-18 22:45:03 +0800

    🦺 Add authentication check in history page server load

--- src/routes/history/+page.server.ts
+++ src/routes/history/+page.server.ts
@@ -1,5 +1,7 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
-    return {};
+    const session = await locals.auth.validate();
+    if (!session) throw redirect(302, '/auth/login');
 }) satisfies PageServerLoad;
