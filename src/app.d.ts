9d14fcfd6869470aac974a3b6e7f0ecdea0b2d62
Peter John Arao <pitzzahh@gmail.com>
2024-01-18 11:17:17 +0800

    🏷️ Update app.d.ts with new interfaces and variables

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,12 +2,25 @@ import { PrismaClient } from '@prisma/client'
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth: import("lucia").AuthRequest,
+			validate: import("@lucia-auth/sveltekit").Validate,
+			validateUser: import("@lucia-auth/sveltekit").ValidateUser,
+			setSession: import("@lucia-auth/sveltekit").SetSession
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
-	var prisma: PrismaClient
+	var __prisma: PrismaClient
+
+	declare namespace Lucia {
+		type Auth = import("$lib/server/lucia").Auth
+		type UserAttributes = {
+			username: string,
+			name: string
+		}
+	}
 }
 
 export { };
