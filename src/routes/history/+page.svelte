a192bc961fd810b71cd2a3dab1695b820d5885b4
Peter John Arao <pitzzahh@gmail.com>
2024-01-23 00:09:20 +0800

    ✨ Update DataTable component prop to use history data

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import DataTable from './data-table.svelte';
+
+	export let data: PageData;
 </script>
 
 <svelte:head>
@@ -8,5 +10,5 @@
 </svelte:head>
 
 <div class="container mx-auto">
-	<DataTable />
+	<DataTable history={data.history}/>
 </div>
