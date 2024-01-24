5ada37400dbcd3af1de2c8760c0471f05f1fc2b6
Peter John Arao <pitzzahh@gmail.com>
2024-01-24 12:33:10 +0800

    🔧 Add "noExternal" option to Vite config for SSR chart.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig({ssr:{
+	noExternal:['chart.js'],
+	},
 	plugins: [sveltekit()]
 });
