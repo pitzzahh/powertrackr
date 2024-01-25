8c47d80ae51dab0e3b0598936412d7557bbb594b
Peter John Arao <pitzzahh@gmail.com>
2024-01-25 13:56:30 +0800

    ♻️ Update date range for sample data generation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -23,7 +23,7 @@ export const generateSampleData = (count: number) => {
     let trendDuration = 0;
 
     for (let i = 0; i < count; i++) {
-        const date = randomDate(new Date(2018, 0, 1), new Date());
+        const date = randomDate(new Date(2002, 0, 1), new Date());
         while (sampleData.some((x) => x.date.getTime() === date.getTime())) {
             date.setDate(randomDate(new Date(2002, 0, 1), new Date()).getDate());
         }
