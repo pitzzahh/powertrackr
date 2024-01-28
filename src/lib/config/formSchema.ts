dd158bd651ad7319e5ddfe4f01289ffa62d3b2b0
Peter John Arao <97332554+pitzzahh@users.noreply.github.com>
2024-01-28 23:30:56 +0800

    🔧 Update bill form schema/format

--- src/lib/config/formSchema.ts
+++ src/lib/config/formSchema.ts
@@ -1,94 +1,103 @@
-import { z } from "zod";
+import { error } from '@sveltejs/kit';
+import { z } from 'zod';
 
 const MIN_LENGTH = 6;
 const FIELD_VALIDATION = {
-    TEST: {
-        SPECIAL_CHAR: (value: string) => /[-._!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+/.test(value),
-        LOWERCASE: (value: string) => /[a-z]/.test(value),
-        UPPERCASE: (value: string) => /[A-Z]/.test(value),
-        NUMBER: (value: string) => /.*[0-9].*/.test(value),
-    },
-    MSG: {
-        MIN_LEN: `Password must have ${MIN_LENGTH} characters`,
-        SPECIAL_CHAR: "Password must contain atleast one special character",
-        LOWERCASE: "Password must contain at least one lowercase letter",
-        UPPERCASE: "Password must contain at least one uppercase letter",
-        NUMBER: "Password must contain at least one number",
-        MATCH: "Password must match",
-    },
+	TEST: {
+		SPECIAL_CHAR: (value: string) => /[-._!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+/.test(value),
+		LOWERCASE: (value: string) => /[a-z]/.test(value),
+		UPPERCASE: (value: string) => /[A-Z]/.test(value),
+		NUMBER: (value: string) => /.*[0-9].*/.test(value)
+	},
+	MSG: {
+		MIN_LEN: `Password must have ${MIN_LENGTH} characters`,
+		SPECIAL_CHAR: 'Password must contain atleast one special character',
+		LOWERCASE: 'Password must contain at least one lowercase letter',
+		UPPERCASE: 'Password must contain at least one uppercase letter',
+		NUMBER: 'Password must contain at least one number',
+		MATCH: 'Password must match'
+	}
 };
 
-export const registerFormSchema = z.object({
-    username: z.string().min(2).max(50),
-    name: z.string(),
-    password: z.string()
-        .min(MIN_LENGTH, {
-            message: FIELD_VALIDATION.MSG.MIN_LEN,
-        })
-        .refine(FIELD_VALIDATION.TEST.SPECIAL_CHAR, FIELD_VALIDATION.MSG.SPECIAL_CHAR)
-        .refine(FIELD_VALIDATION.TEST.LOWERCASE, FIELD_VALIDATION.MSG.LOWERCASE)
-        .refine(FIELD_VALIDATION.TEST.UPPERCASE, FIELD_VALIDATION.MSG.UPPERCASE)
-        .refine(FIELD_VALIDATION.TEST.NUMBER, FIELD_VALIDATION.MSG.NUMBER),
-    confirmPassword: z.string().min(MIN_LENGTH),
-}).superRefine(({ confirmPassword, password }, ctx) => {
-    if (confirmPassword !== password) {
-        addFieldIssue("password", ctx);
-        addFieldIssue("confirmPassword", ctx);
-    }
-});
+export const registerFormSchema = z
+	.object({
+		username: z.string().min(2).max(50),
+		name: z.string(),
+		password: z
+			.string()
+			.min(MIN_LENGTH, {
+				message: FIELD_VALIDATION.MSG.MIN_LEN
+			})
+			.refine(FIELD_VALIDATION.TEST.SPECIAL_CHAR, FIELD_VALIDATION.MSG.SPECIAL_CHAR)
+			.refine(FIELD_VALIDATION.TEST.LOWERCASE, FIELD_VALIDATION.MSG.LOWERCASE)
+			.refine(FIELD_VALIDATION.TEST.UPPERCASE, FIELD_VALIDATION.MSG.UPPERCASE)
+			.refine(FIELD_VALIDATION.TEST.NUMBER, FIELD_VALIDATION.MSG.NUMBER),
+		confirmPassword: z.string().min(MIN_LENGTH)
+	})
+	.superRefine(({ confirmPassword, password }, ctx) => {
+		if (confirmPassword !== password) {
+			addFieldIssue('password', ctx);
+			addFieldIssue('confirmPassword', ctx);
+		}
+	});
 
-const addFieldIssue = (field: string, ctx: { addIssue: any; path?: (string | number)[]; }) => {
-    ctx.addIssue({
-        code: "custom",
-        message: FIELD_VALIDATION.MSG.MATCH,
-        path: [field],
-        fatal: true,
-    });
+const addFieldIssue = (field: string, ctx: { addIssue: any; path?: (string | number)[] }) => {
+	ctx.addIssue({
+		code: 'custom',
+		message: FIELD_VALIDATION.MSG.MATCH,
+		path: [field],
+		fatal: true
+	});
 };
 
 export const loginFormSchema = z.object({
-    username: z.string().min(2).max(50),
-    password: z.string()
-        .min(MIN_LENGTH, {
-            message: FIELD_VALIDATION.MSG.MIN_LEN,
-        })
-})
+	username: z.string().min(2).max(50),
+	password: z.string().min(MIN_LENGTH, {
+		message: FIELD_VALIDATION.MSG.MIN_LEN
+	})
+});
 
 export const billFormSchema = z.object({
-    balance: z.preprocess(
-        (input) => {
-            const processed = z.string().regex(/^\d+$/).transform(Number).safeParse(input);
-            return processed.success ? processed.data : input;
-        },
-        z.number().min(0, { message: "Balance must be greater than 0" }),
-    ),
-    totalKwh: z.preprocess(
-        (input) => {
-            const processed = z.string().regex(/^\d+$/).transform(Number).safeParse(input);
-            return processed.success ? processed.data : input;
-        },
-        z.number().min(0, { message: "Total kWh must be greater than 0" }),
-    ),
-    subReading: z.preprocess(
-        (input) => {
-            const processed = z.string().regex(/^\d+$/).transform(Number).safeParse(input);
-            return processed.success ? processed.data : input;
-        },
-        z.number()
-            .min(0, { message: "Sub-reading must be greater than 0" })
-            .or(z.literal(null)),
-    ),
-    payment: z.preprocess(
-        (input) => {
-            const processed = z.string().regex(/^\d+$/).transform(Number).safeParse(input);
-            return processed.success ? processed.data : input;
-        },
-        z.number()
-            .min(0, { message: "Payment must be greater than 0" })
-            .or(z.literal(null)),
-    ),
-    status: z.boolean()
-})
+	balance: z.string()
+	.refine(FIELD_VALIDATION.TEST.NUMBER, 'Balance must be a number')
+	.refine(
+		(value) => {
+			if (isNaN(Number(value))) {
+				throw error(400, 'Balance must be a number');
+			}
+			const b = Number(value);
+			return b > 0;
+		},
+		{ message: 'Balance must be greater than 0' }
+	),
+	totalKwh: z.string()
+	.refine(FIELD_VALIDATION.TEST.NUMBER, 'Total Kwh must be a number')
+	.refine(
+		(value) => {
+			if (isNaN(Number(value))) {
+				throw error(400, 'Total Kwh must be a number');
+			}
+			const b = Number(value);
+			return b > 0;
+		},
+		{ message: 'Total Kwh must be greater than 0' }
+	),
+	subReading: z
+		.string()
+		.refine(FIELD_VALIDATION.TEST.NUMBER, 'SubReading must be a number')
+		.refine(
+			(value) => {
+				if (isNaN(Number(value))) {
+					throw error(400, 'SubReading must be a number');
+				}
+				const b = Number(value);
+				return b > 0;
+			},
+			{ message: 'SubReading must be greater than 0' }
+		)
+		.optional(),
+	status: z.boolean()
+});
 
 export type RegisterFormSchema = typeof registerFormSchema;
 export type LoginFormSchema = typeof loginFormSchema;
