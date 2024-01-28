37a09e456b61394750d8b185334ca6182dc8acdc
Peter John Arao <97332554+pitzzahh@users.noreply.github.com>
2024-01-28 23:41:01 +0800

    ♻️ Refactor history data prop data

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
+	import { formatDate } from '$lib';
 	import type { PageData } from './$types';
 	import DataTable from './data-table.svelte';
 
 	export let data: PageData;
+
+	$: history = data.user?.billing_info.map((bill) => {
+		return {
+			id: bill.id,
+			date: formatDate(new Date(bill.date)),
+			totalKwh: bill.totalKwh,
+			subKwh: bill.subKwh,
+			payPerKwh: bill.payPerKwh,
+			balance: bill.balance,
+			payment: bill.payment?.amount,
+			subPayment: bill.subPayment?.amount,
+			status: bill.status
+		};
+	}) as BillingInfoDTO[];
+
+	$: console.log(`History Data: ${JSON.stringify(history, null, 2)}`)
 </script>
 
 <svelte:head>
@@ -10,5 +27,5 @@
 </svelte:head>
 
 <div class="container mx-auto">
-	<DataTable history={data.history}/>
+	<DataTable {history} />
 </div>
