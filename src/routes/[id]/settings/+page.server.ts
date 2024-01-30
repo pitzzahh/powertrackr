32184ccf570d4966e044e8375bea808591a2da76
Peter John Arao <pitzzahh@gmail.com>
2024-01-30 23:16:17 +0800

    ♻️ Removed throw on redirect and error with sveltekit new navigation stuff

--- src/routes/[id]/settings/+page.server.ts
+++ src/routes/[id]/settings/+page.server.ts
@@ -3,18 +3,17 @@ import type { PageServerLoad } from './$types';
 import { prismaClient } from '$lib/server/prisma';
 
 export const load = (async ({ locals, params }) => {
-    const session = await locals.auth.validate();
-    if (!session) throw redirect(302, '/auth/login');
+	const session = await locals.auth.validate();
+	if (!session) redirect(302, '/auth/login');
 
-    const user = await prismaClient.user.findUnique({
-        where: {
-            username: params.id
-        }
-    });
+	const user = await prismaClient.user.findUnique({
+		where: {
+			username: params.id
+		}
+	});
 
-    if (!user || user.id !== session.user.userId) {
-        console.error('User not found or not logged in as user');
-        throw error(403, 'You do not have permission to access that page');
-    }
-
+	if (!user || user.id !== session.user.userId) {
+		console.error('User not found or not logged in as user');
+		error(403, 'You do not have permission to access that page');
+	}
 }) satisfies PageServerLoad;
--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -1,57 +1,57 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
-import { fail } from "@sveltejs/kit";
-import { superValidate } from "sveltekit-superforms/server";
+import { fail } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms/server';
 import { registerFormSchema } from '$lib/config/formSchema';
 import { auth } from '$lib/server/lucia';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 export const load = (async ({ locals }) => {
-    const session = await locals.auth.validate();
-    if (session) throw redirect(302, '/');
-    return {
-        form: await superValidate(registerFormSchema)
-    }
+	const session = await locals.auth.validate();
+	if (session) redirect(302, '/');
+	return {
+		form: await superValidate(registerFormSchema)
+	};
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
-    default: async (event) => {
-        const registrationForm = await superValidate(event, registerFormSchema);
-        if (!registrationForm.valid) {
-            return fail(400, {
-                form: registrationForm
-            });
-        }
-        
-        const { name, username, password } = registrationForm.data;
+	default: async (event) => {
+		const registrationForm = await superValidate(event, registerFormSchema);
+		if (!registrationForm.valid) {
+			return fail(400, {
+				form: registrationForm
+			});
+		}
 
-        try {
-            await auth.createUser({
-                key: {
-                    providerId: 'username',
-                    providerUserId: username,
-                    password
-                },
-                attributes: {
-                    name,
-                    username,
-                    picture: null
-                }
-            })
+		const { name, username, password } = registrationForm.data;
+
+		try {
+			await auth.createUser({
+				key: {
+					providerId: 'username',
+					providerUserId: username,
+					password
+				},
+				attributes: {
+					name,
+					username,
+					picture: null
+				}
+			});
 		} catch (e: any) {
 			// check for unique constraint error in user table
-            console.error(`e: ${JSON.stringify(e)}`)
+			console.error(`e: ${JSON.stringify(e)}`);
 			if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
 				return fail(400, {
 					message: 'Username already taken',
-                    form: registrationForm
+					form: registrationForm
 				});
 			}
 			return fail(500, {
 				message: 'An unknown error occurred',
-                form: registrationForm
+				form: registrationForm
 			});
 		}
-        throw redirect(302, '/auth/login');
-    }
+		redirect(301, '/auth/login');
+	}
 };
