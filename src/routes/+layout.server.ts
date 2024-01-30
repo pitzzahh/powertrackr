a5e1a9cd96514efe7a4808023411523074e3a44f
Peter John Arao <pitzzahh@gmail.com>
2024-01-30 23:10:20 +0800

    ♻️ Separate user from bill info returned layout data

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,25 +1,42 @@
 import type { Session } from 'lucia';
 import type { LayoutServerLoad } from './$types';
 import { prismaClient } from '$lib/server/prisma';
+import type { BillingInfo, User, Payment } from '@prisma/client';
+import type { ExtendedBillingInfo } from '$lib/types';
 
 export const load = (async ({ locals }) => {
 	const session: Session = await locals.auth.validate();
 	if (session) {
-		const user = await prismaClient.user.findUnique({
+		const user = (await prismaClient.user.findUnique({
 			where: {
 				id: session.user.userId
+			}
+		})) as User;
+
+		const history = (await prismaClient.billingInfo.findMany({
+			where: {
+				user_id: user.id
 			},
 			include: {
-				billing_info: {
-					include: {
-						payment: true,
-						subPayment: true
-					}
-				}
+				payment: true,
+				subPayment: true
 			}
-		});
+		})) as ExtendedBillingInfo[] | undefined;
+
+		console.log(
+			`REFETCHING ALL: ${JSON.stringify(
+				{
+					user,
+					history
+				},
+				null,
+				2
+			)}`
+		);
+
 		return {
-			user
+			user,
+			history
 		};
 	}
 }) satisfies LayoutServerLoad;
