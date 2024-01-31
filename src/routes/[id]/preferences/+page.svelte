f8d8983f71a2d14b2251d3466341d45261b52507
Peter John Arao <pitzzahh@gmail.com>
2024-01-31 19:57:40 +0800

    🏗️ Add preferences route

---
+++ src/routes/[id]/preferences/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>{data.user?.name} preference settings</title>
+</svelte:head>
+
+<h4>Preferences</h4>
+<p class="text-sm text-muted-foreground [&:not(:first-child)]:mt-1 md:[&:not(:first-child)]:mt-1 sm:mb-6">
+	Customize the app based on your preferences such as themes etc.
+</p>
