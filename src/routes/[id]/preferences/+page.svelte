1f6ba5c59bf894f2bb8a8687c67ef999f0de695a
Peter John Arao <pitzzahh@gmail.com>
2024-01-31 23:09:26 +0800

    💄 Update styling

--- src/routes/[id]/preferences/+page.svelte
+++ src/routes/[id]/preferences/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { Separator } from '$lib/components/ui/separator';
 
 	export let data: PageData;
 </script>
@@ -12,3 +13,5 @@
 <p class="text-sm text-muted-foreground [&:not(:first-child)]:mt-1 md:[&:not(:first-child)]:mt-1 sm:mb-6">
 	Customize the app based on your preferences such as themes etc.
 </p>
+
+<Separator class="my-4" />
