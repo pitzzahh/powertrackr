cb5cd3ad4bb63122a65e99e9fc8ceb04db01bb6a
Peter John Arao <pitzzahh@gmail.com>
2024-01-31 20:02:24 +0800

    ♻️ Refactor state management functions to have context argument

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -2,14 +2,14 @@ import { getContext, setContext } from 'svelte';
 import { writable, type Writable } from 'svelte/store';
 import type { State } from '$lib/types';
 
-const STATE_CTX = 'STATE_CTX';
+export const STATE_CTX = 'STATE_CTX';
 
-export const setState = (initialData: State) => {
+export const setState = (initialData: State, context: string) => {
 	const state = writable<State>(initialData);
 	setContext(STATE_CTX, state);
 	return state;
 };
 
-export const getState = () => {
-    return getContext<Writable<State>>(STATE_CTX);
+export const getState = (context: string) => {
+    return getContext<Writable<State>>(context);
 }
