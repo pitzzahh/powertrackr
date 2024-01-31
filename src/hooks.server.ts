db6b809b2057e9a0db9de39109c0e8ca0b92c2ab
Peter John Arao <pitzzahh@gmail.com>
2024-01-31 19:54:30 +0800

    ✨ Refactor hooks to use sequential handling

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
-import { auth } from "$lib/server/lucia";
-import type { Handle } from "@sveltejs/kit";
+import { auth } from '$lib/server/lucia';
+import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handleLucia: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
 	return await resolve(event);
 };
+
+export const handle: Handle = sequence(handleLucia);
