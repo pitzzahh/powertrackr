9ea0244611a8e04d5b77eb552f488c9759a653fd
Peter John Arao <pitzzahh@gmail.com>
2024-02-01 21:46:56 +0800

    ✨ Add i18n hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,13 @@
 import { auth } from '$lib/server/lucia';
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { i18n } from "$lib/i18n"
 
 export const handleLucia: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
 	return await resolve(event);
 };
 
-export const handle: Handle = sequence(handleLucia);
+export const handleI18n = i18n.handle()
+
+export const handle: Handle = sequence(handleI18n, handleLucia);
