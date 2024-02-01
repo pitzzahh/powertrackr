c86bf5bbde214046099b353e78dc1b66acd59a84
Peter John Arao <pitzzahh@gmail.com>
2024-02-01 16:30:26 +0800

    🎨 Format code

--- src/routes/[id]/preferences/+page.svelte
+++ src/routes/[id]/preferences/+page.svelte
@@ -10,7 +10,9 @@
 </svelte:head>
 
 <h4>Preferences</h4>
-<p class="text-sm text-muted-foreground [&:not(:first-child)]:mt-1 md:[&:not(:first-child)]:mt-1 sm:mb-6">
+<p
+	class="text-sm text-muted-foreground sm:mb-6 [&:not(:first-child)]:mt-1 md:[&:not(:first-child)]:mt-1"
+>
 	Customize the app based on your preferences such as themes etc.
 </p>
 
