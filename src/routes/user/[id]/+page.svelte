23b163f88ef8203dc6bbad478460f6344b47cd96
Peter John Arao <pitzzahh@gmail.com>
2024-02-03 23:49:40 +0800

    🍱 Add default avatar assets

--- src/routes/user/[id]/+page.svelte
+++ src/routes/user/[id]/+page.svelte
@@ -10,8 +10,12 @@
 	import type { FormOptions } from 'formsnap';
 	import { toast } from 'svelte-sonner';
 	import { Loader2 } from 'lucide-svelte';
+	import * as RadioGroup from '$lib/components/ui/radio-group';
+	import { Label } from '$lib/components/ui/label';
 	import { Separator } from '$lib/components/ui/separator';
 	import * as m from '$paraglide/messages';
+	import { cn } from '$lib/utils';
+	import { defaultUserAvatars } from '$lib/assets/default-user-avatar';
 
 	export let data: PageData;
 
@@ -20,6 +24,7 @@
 	let selectedFile: File | undefined;
 	let avatarSrc = $state.user?.picture || '';
 	$: processing = false;
+	$: currentAvatar = avatarSrc || 'user1';
 
 	const options: FormOptions<ProfileFormSchema> = {
 		onSubmit() {
@@ -83,8 +88,8 @@
 	let:config
 >
 	<Form.Field {config} name="picture">
-		<Form.Item class="group relative flex flex-col items-start">
-			<label for="avatarInput" class="relative cursor-pointer">
+		<Form.Item class="relative flex items-start">
+			<Label for="avatarInput" class="relative">
 				<Avatar.Root class="m-2 h-32 w-32">
 					<Avatar.Image src={avatarSrc} alt="@{$state.user?.username}" />
 					<Avatar.Fallback class="text-xl">{getInitials($state.user?.name)}</Avatar.Fallback>
@@ -97,18 +102,51 @@
 					on:change={handleFileChange}
 				/>
 				<span
-					class="absolute bottom-6 w-full transform text-center text-sm text-muted-foreground opacity-100 transition-opacity sm:opacity-0 sm:group-hover:opacity-100"
+					class="absolute bottom-6 left-[2.2rem] transform text-center text-sm text-muted-foreground opacity-100 transition-opacity hover:cursor-pointer sm:opacity-0 sm:hover:opacity-100"
 				>
 					Choose File
 				</span>
-			</label>
+			</Label>
+			<div class="grid h-40 grid-flow-row auto-rows-max gap-1 overflow-auto scrollbar-hide">
+				<RadioGroup.Root
+					value={currentAvatar}
+					class="flex flex-wrap items-center justify-center gap-2"
+					onValueChange={async (val) => {
+						currentAvatar = val;
+						// @ts-ignore
+						avatarSrc = defaultUserAvatars.find((avatar) => avatar.id === Number(currentAvatar))?.src.img.src || '';
+					}}
+				>
+					{#each defaultUserAvatars as avatar}
+						<Label
+							for={avatar.id.toString()}
+							class="h-20 w-20 rounded-full bg-popover p-1 hover:cursor-pointer hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+						>
+							<enhanced:img
+								src={avatar.src}
+								alt={avatar.id.toString()}
+								style="width:100%; height:100%; object-fit:cover border-radius:9999px;"
+							/>
+							<RadioGroup.Item
+								value={avatar.id.toString()}
+								id={avatar.id.toString()}
+								class="sr-only"
+								aria-label={avatar.id.toString()}
+							/>
+						</Label>
+					{/each}
+				</RadioGroup.Root>
+			</div>
+
 			<Form.Validation />
 		</Form.Item>
 	</Form.Field>
-	<Form.Button disabled={processing}>
-		{#if processing}
-			<Loader2 class="mr-2 h-4 w-4 animate-spin" />
-		{/if}
-		{processing ? 'Please wait' : 'Save'}</Form.Button
-	>
+	{#if avatarSrc !== $state.user?.picture}
+		<Form.Button disabled={processing}>
+			{#if processing}
+				<Loader2 class="mr-2 h-4 w-4 animate-spin" />
+			{/if}
+			{processing ? 'Please wait' : 'Save'}</Form.Button
+		>
+	{/if}
 </Form.Root>
