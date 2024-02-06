557f1d8578bf0f1c1da2563ad5f4a70cd5fff24f
Peter John Arao <pitzzahh@gmail.com>
2024-02-07 00:18:31 +0800

    🔥 Undo prerender /user/[id]/preferences route

--- src/routes/user/[id]/preferences/+page.server.ts
+++ src/routes/user/[id]/preferences/+page.server.ts
@@ -1,8 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const prerender = true;
-
 export const load = (async ({locals}) => {
 	const session = await locals.auth.validate();
 	if (!session) redirect(302, '/auth/login');
