9f85705f3b2fe2668b5fb81277e30e9f7802580d
Peter John Arao <pitzzahh@gmail.com>
2024-02-07 22:20:17 +0800

    💄 Refactor RadioGroup to  ComboBox

--- src/routes/user/[id]/preferences/+page.svelte
+++ src/routes/user/[id]/preferences/+page.svelte
@@ -1,14 +1,53 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { Separator } from '$lib/components/ui/separator';
-	import { lang } from '$lib';
+	import { getLanguageName, lang } from '$lib';
 	import { availableLanguageTags, languageTag, setLanguageTag } from '$paraglide/runtime';
 	import { Label } from '$lib/components/ui/label';
 	import * as RadioGroup from '$lib/components/ui/radio-group';
 	import * as m from '$paraglide/messages';
 	import { goto } from '$app/navigation';
+	import * as Select from '$lib/components/ui/select';
+	import { Check, ChevronsUpDown } from 'lucide-svelte';
+	import * as Command from '$lib/components/ui/command';
+	import * as Popover from '$lib/components/ui/popover';
+	import { Button } from '$lib/components/ui/button';
+	import { cn } from '$lib/utils';
+	import { tick } from 'svelte';
 
 	export let data: PageData;
+
+	const languages = availableLanguageTags
+		.map((lang) => {
+			return {
+				label: getLanguageName(lang)!,
+				value: lang
+			};
+		})
+		.sort((a, b) => a.label.localeCompare(b.label, undefined, { sensitivity: 'base' }));
+
+	let open = false;
+	let value = '';
+	$: selectedValue = languages.find((lang) => lang.value === value) ?? 'Select a language...';
+
+	$: {
+		if (selectedValue !== 'Select a language...') {
+			console.log(`Selected language: ${selectedValue}`);
+			// @ts-ignore
+			setLanguageTag(selectedValue.value);
+			$lang = languageTag();
+			goto('/').then(() => goto(`user/${data.user?.username}/preferences`));
+		}
+	}
+	// We want to refocus the trigger button when the user selects
+	// an item from the list so users can continue navigating the
+	// rest of the form with the keyboard.
+	const closeAndFocusTrigger = (triggerId: string): void => {
+		open = false;
+		tick().then(() => {
+			document.getElementById(triggerId)?.focus();
+		});
+	};
 </script>
 
 <svelte:head>
@@ -25,26 +64,41 @@
 <Separator />
 <p class="text-md font-medium leading-none">Language</p>
 <span class=" text-sm text-muted-foreground">Choose your preferred language</span>
-<div class="mt-2 flex gap-2">
-	<RadioGroup.Root
-		value={languageTag()}
-		class="flex items-center justify-start gap-4"
-		onValueChange={async (val) => {
-			// @ts-ignore
-			setLanguageTag(val);
-			$lang = languageTag();
-			await goto('/');
-			goto(`user/${data.user?.username}/preferences`);
-		}}
-	>
-		{#each availableLanguageTags as lang}
-			<Label
-				for={lang}
-				class="rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+<div class="mt-2">
+	<Popover.Root bind:open let:ids>
+		<Popover.Trigger asChild let:builder>
+			<Button
+				builders={[builder]}
+				variant="outline"
+				role="combobox"
+				aria-expanded={open}
+				class="w-1/2 justify-between"
 			>
-				<RadioGroup.Item value={lang} id={lang} class="sr-only" aria-label={lang} />
-				{lang}
-			</Label>
-		{/each}
-	</RadioGroup.Root>
+				{getLanguageName(languageTag())}
+				<ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+			</Button>
+		</Popover.Trigger>
+		<Popover.Content class="w-1/2 p-0">
+			<Command.Root>
+				<Command.Input placeholder="Search Language..." />
+				<Command.Empty>No language found.</Command.Empty>
+				<Command.Group>
+					{#each languages as lang}
+						<Command.Item
+							value={lang.value}
+							onSelect={(currentValue) => {
+								value = currentValue;
+								closeAndFocusTrigger(ids.trigger);
+							}}
+						>
+							<Check
+								class={cn('mr-2 h-4 w-4', languageTag() !== lang.value && 'text-transparent')}
+							/>
+							{lang.label}
+						</Command.Item>
+					{/each}
+				</Command.Group>
+			</Command.Root>
+		</Popover.Content>
+	</Popover.Root>
 </div>
