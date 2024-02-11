e47f61150def5d97226d2338dfabf3d3b0f1c0b2
Peter John Arao <pitzzahh@gmail.com>
2024-02-11 22:04:08 +0800

    🐛 Fix ogImage url

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
 	name: 'powertrackr',
 	url: 'https://powertrackr.vercel.app',
-	ogImage: 'https://powertrackr.vercel.app/og.png',
+	ogImage: 'https://powertrackr.vercel.app/og.avif',
 	description: 'Monitor and track electricity consumption.',
 	keywords: `e-bill, electricity, power, track, consumption, monitor, bill`
 };
