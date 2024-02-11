7ade222599e73528fa17eff0a1083861b0f76d26
Peter John Arao <pitzzahh@gmail.com>
2024-02-11 21:34:33 +0800

    💄 Refactor landing page

--- src/lib/components/landing-page.svelte
+++ src/lib/components/landing-page.svelte
@@ -4,16 +4,16 @@
 	import { app_desc, get_started } from '$paraglide/messages';
 </script>
 
-<div class="container">
-	<div class=" flex items-center justify-center lg:-translate-y-28">
+<div class="md:-translate-y-24 lg:-translate-y-28">
+	<div class="flex items-center justify-center">
 		<div class="flex flex-col gap-10">
-			<h1 class="mt-10 text-wrap text-center sm:text-left">{app_desc()}</h1>
+			<h1 class="text-wrap text-center sm:text-left">{app_desc()}</h1>
 			<Button href="/auth/register">{get_started()}</Button>
 		</div>
 		<img
 			src={illustration}
 			alt="illustration"
-			class="md:h[40rem] hidden sm:block sm:h-[20rem] lg:h-[50rem]"
+			class="md:h[34rem] hidden sm:block sm:h-[20rem] lg:h-[44rem]"
 		/>
 	</div>
 	<div></div>
