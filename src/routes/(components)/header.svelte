77807c6fffcc0174ed9af2b59970288165a4c052
pitzzahh <pitzzahh@gmail.com>
2025-12-23 23:02:55 +0800

    wip: Header component with theme toggle and menu

---
+++ src/routes/(components)/header.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { Button } from "$/components/ui/button";
+  import * as DropdownMenu from "$/components/ui/dropdown-menu";
+  import { MoonIcon, SunIcon } from "@lucide/svelte";
+  import { toggleMode } from "mode-watcher";
+</script>
+
+<header
+  class="absolute top-0 left-0 right-0 z-50 flex items-center justify-between p-6 bg-background/10 backdrop-blur-[120px]"
+>
+  <!-- <AppLogo class="text-white h-8 w-auto" /> -->
+  <Button onclick={toggleMode} variant="outline" size="icon">
+    <SunIcon
+      class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
+    />
+    <MoonIcon
+      class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
+    />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
+
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger>
+      {#snippet child({ props })}
+        <Button {...props} variant="outline">Open</Button>
+      {/snippet}
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content class="w-56" align="start">
+      <DropdownMenu.Label>My Account</DropdownMenu.Label>
+      <DropdownMenu.Group>
+        <DropdownMenu.Item>
+          Profile
+          <DropdownMenu.Shortcut>⇧⌘P</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+        <DropdownMenu.Item>
+          Billing
+          <DropdownMenu.Shortcut>⌘B</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+        <DropdownMenu.Item>
+          Settings
+          <DropdownMenu.Shortcut>⌘S</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+        <DropdownMenu.Item>
+          Keyboard shortcuts
+          <DropdownMenu.Shortcut>⌘K</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+      </DropdownMenu.Group>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Group>
+        <DropdownMenu.Item>Team</DropdownMenu.Item>
+        <DropdownMenu.Sub>
+          <DropdownMenu.SubTrigger>Invite users</DropdownMenu.SubTrigger>
+          <DropdownMenu.SubContent>
+            <DropdownMenu.Item>Email</DropdownMenu.Item>
+            <DropdownMenu.Item>Message</DropdownMenu.Item>
+            <DropdownMenu.Separator />
+            <DropdownMenu.Item>More...</DropdownMenu.Item>
+          </DropdownMenu.SubContent>
+        </DropdownMenu.Sub>
+        <DropdownMenu.Item>
+          New Team
+          <DropdownMenu.Shortcut>⌘+T</DropdownMenu.Shortcut>
+        </DropdownMenu.Item>
+      </DropdownMenu.Group>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item>GitHub</DropdownMenu.Item>
+      <DropdownMenu.Item>Support</DropdownMenu.Item>
+      <DropdownMenu.Item disabled>API</DropdownMenu.Item>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item>
+        Log out
+        <DropdownMenu.Shortcut>⇧⌘Q</DropdownMenu.Shortcut>
+      </DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
+</header>
