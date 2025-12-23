7534bada61fe077e953e22ccf7510dc56dc70cb9
pitzzahh <pitzzahh@gmail.com>
2025-12-23 23:02:00 +0800

    feat: add dropdown menu component

---
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-label.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { WithElementRef } from "$/index";
+  import { cn } from "$lib/utils/style.js";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    inset,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> & {
+    inset?: boolean;
+  } = $props();
+</script>
+
+<div
+  bind:this={ref}
+  data-slot="dropdown-menu-label"
+  data-inset={inset}
+  class={cn("px-2 py-1.5 text-sm font-semibold data-[inset]:ps-8", className)}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
---
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-shortcut.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+  import { cn } from "$lib/utils/style.js";
+  import type { WithElementRef } from "$/index";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
+</script>
+
+<span
+  bind:this={ref}
+  data-slot="dropdown-menu-shortcut"
+  class={cn("text-muted-foreground ms-auto text-xs tracking-widest", className)}
+  {...restProps}
+>
+  {@render children?.()}
+</span>
