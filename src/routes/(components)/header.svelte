786c20d6f4efd557039107c2be70f7398cafc9c9
pitzzahh <pitzzahh@gmail.com>
2025-12-23 23:50:40 +0800

    refactor: add logo and btn group theme and user controls

--- src/routes/(components)/header.svelte
+++ src/routes/(components)/header.svelte
@@ -1,76 +1,85 @@
 <script lang="ts">
+  import Logo from "$/components/logo.svelte";
   import { Button } from "$/components/ui/button";
   import * as DropdownMenu from "$/components/ui/dropdown-menu";
-  import { MoonIcon, SunIcon } from "@lucide/svelte";
+  import * as ButtonGroup from "$/components/ui/button-group/index.js";
+  import { MoonIcon, SunIcon, User } from "@lucide/svelte";
   import { toggleMode } from "mode-watcher";
 </script>
 
 <header
-  class="absolute top-0 left-0 right-0 z-50 flex items-center justify-between p-6 bg-background/10 backdrop-blur-[120px]"
+  class="absolute top-0 left-0 right-0 z-50 flex items-center justify-between p-4 bg-background/10 backdrop-blur-[120px]"
 >
-  <!-- <AppLogo class="text-white h-8 w-auto" /> -->
-  <Button onclick={toggleMode} variant="outline" size="icon">
-    <SunIcon
-      class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
-    />
-    <MoonIcon
-      class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
-    />
-    <span class="sr-only">Toggle theme</span>
-  </Button>
+  <Logo variant="ghost" class="px-0" />
 
-  <DropdownMenu.Root>
-    <DropdownMenu.Trigger>
-      {#snippet child({ props })}
-        <Button {...props} variant="outline">Open</Button>
-      {/snippet}
-    </DropdownMenu.Trigger>
-    <DropdownMenu.Content class="w-56" align="start">
-      <DropdownMenu.Label>My Account</DropdownMenu.Label>
-      <DropdownMenu.Group>
-        <DropdownMenu.Item>
-          Profile
-          <DropdownMenu.Shortcut>⇧⌘P</DropdownMenu.Shortcut>
-        </DropdownMenu.Item>
-        <DropdownMenu.Item>
-          Billing
-          <DropdownMenu.Shortcut>⌘B</DropdownMenu.Shortcut>
-        </DropdownMenu.Item>
-        <DropdownMenu.Item>
-          Settings
-          <DropdownMenu.Shortcut>⌘S</DropdownMenu.Shortcut>
-        </DropdownMenu.Item>
-        <DropdownMenu.Item>
-          Keyboard shortcuts
-          <DropdownMenu.Shortcut>⌘K</DropdownMenu.Shortcut>
-        </DropdownMenu.Item>
-      </DropdownMenu.Group>
-      <DropdownMenu.Separator />
-      <DropdownMenu.Group>
-        <DropdownMenu.Item>Team</DropdownMenu.Item>
-        <DropdownMenu.Sub>
-          <DropdownMenu.SubTrigger>Invite users</DropdownMenu.SubTrigger>
-          <DropdownMenu.SubContent>
-            <DropdownMenu.Item>Email</DropdownMenu.Item>
-            <DropdownMenu.Item>Message</DropdownMenu.Item>
-            <DropdownMenu.Separator />
-            <DropdownMenu.Item>More...</DropdownMenu.Item>
-          </DropdownMenu.SubContent>
-        </DropdownMenu.Sub>
-        <DropdownMenu.Item>
-          New Team
-          <DropdownMenu.Shortcut>⌘+T</DropdownMenu.Shortcut>
-        </DropdownMenu.Item>
-      </DropdownMenu.Group>
-      <DropdownMenu.Separator />
-      <DropdownMenu.Item>GitHub</DropdownMenu.Item>
-      <DropdownMenu.Item>Support</DropdownMenu.Item>
-      <DropdownMenu.Item disabled>API</DropdownMenu.Item>
-      <DropdownMenu.Separator />
-      <DropdownMenu.Item>
-        Log out
-        <DropdownMenu.Shortcut>⇧⌘Q</DropdownMenu.Shortcut>
-      </DropdownMenu.Item>
-    </DropdownMenu.Content>
-  </DropdownMenu.Root>
+  <div class="flex items-center justify-center">
+    <ButtonGroup.Root>
+      <Button onclick={toggleMode} variant="secondary" size="icon">
+        <SunIcon
+          class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
+        />
+        <MoonIcon
+          class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
+        />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+      <ButtonGroup.Separator />
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          {#snippet child({ props })}
+            <Button {...props} variant="secondary" size="icon">
+              <User />
+            </Button>
+          {/snippet}
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content class="w-56" align="start">
+          <DropdownMenu.Label>My Account</DropdownMenu.Label>
+          <DropdownMenu.Group>
+            <DropdownMenu.Item>
+              Profile
+              <DropdownMenu.Shortcut>⇧⌘P</DropdownMenu.Shortcut>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item>
+              Billing
+              <DropdownMenu.Shortcut>⌘B</DropdownMenu.Shortcut>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item>
+              Settings
+              <DropdownMenu.Shortcut>⌘S</DropdownMenu.Shortcut>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item>
+              Keyboard shortcuts
+              <DropdownMenu.Shortcut>⌘K</DropdownMenu.Shortcut>
+            </DropdownMenu.Item>
+          </DropdownMenu.Group>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Group>
+            <DropdownMenu.Item>Team</DropdownMenu.Item>
+            <DropdownMenu.Sub>
+              <DropdownMenu.SubTrigger>Invite users</DropdownMenu.SubTrigger>
+              <DropdownMenu.SubContent>
+                <DropdownMenu.Item>Email</DropdownMenu.Item>
+                <DropdownMenu.Item>Message</DropdownMenu.Item>
+                <DropdownMenu.Separator />
+                <DropdownMenu.Item>More...</DropdownMenu.Item>
+              </DropdownMenu.SubContent>
+            </DropdownMenu.Sub>
+            <DropdownMenu.Item>
+              New Team
+              <DropdownMenu.Shortcut>⌘+T</DropdownMenu.Shortcut>
+            </DropdownMenu.Item>
+          </DropdownMenu.Group>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item>GitHub</DropdownMenu.Item>
+          <DropdownMenu.Item>Support</DropdownMenu.Item>
+          <DropdownMenu.Item disabled>API</DropdownMenu.Item>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item>
+            Log out
+            <DropdownMenu.Shortcut>⇧⌘Q</DropdownMenu.Shortcut>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </ButtonGroup.Root>
+  </div>
 </header>
