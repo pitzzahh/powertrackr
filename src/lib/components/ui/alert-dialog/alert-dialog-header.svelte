0fbdee8d48535bd9b0c36fbb8e46b14a0c6b06f5
pitzzahh <pitzzahh@gmail.com>
2025-12-24 01:20:52 +0800

    feat: add alert dialog component

---
+++ src/lib/components/ui/alert-dialog/alert-dialog-header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+  import { cn } from "$lib/utils/style.js";
+  import type { WithElementRef } from "$/index";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div
+  bind:this={ref}
+  data-slot="alert-dialog-header"
+  class={cn("flex flex-col gap-2 text-center sm:text-start", className)}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
