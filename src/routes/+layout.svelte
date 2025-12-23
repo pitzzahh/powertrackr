e1b3ffec224b2ed3ad3cb7122cc1f7d24bd0ee3b
pitzzahh <pitzzahh@gmail.com>
2025-12-23 13:54:35 +0800

    chore: setup shadcn-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.svg";
+	import { ModeWatcher } from "mode-watcher";
 
 	const { children } = $props();
 </script>
+<ModeWatcher/>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon}>
+</svelte:head>
 {@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,13 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    alias: {
-      "$/*": "./src/lib/*",
-    },
-    adapter: adapter(),
-  },
+	preprocess: vitePreprocess(),
+	kit: {
+		alias: {
+			"$/*": "./src/lib/*",
+		},
+		adapter: adapter(),
+	},
 };
 
 export default config;
