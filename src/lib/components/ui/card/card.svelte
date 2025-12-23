26c41d848054eb1472d16b662f5afdcb7bb698fe
pitzzahh <pitzzahh@gmail.com>
2025-12-24 01:12:46 +0800

    feat: add card component

---
+++ src/lib/components/ui/card/card.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+  import { cn } from "$lib/utils/style.js";
+  import type { WithElementRef } from "$/index";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div
+  bind:this={ref}
+  data-slot="card"
+  class={cn(
+    "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+    className,
+  )}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
