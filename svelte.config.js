64d7751d0ae89a07504f802878907ef7ffbadaad
pitzzahh <pitzzahh@gmail.com>
2025-12-24 19:30:13 +0800

    feat: add $routes alias to SvelteKit config

--- svelte.config.js
+++ svelte.config.js
@@ -3,21 +3,22 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		alias: {
-			"$/*": "./src/lib/*",
-		},
-		experimental: {
-			remoteFunctions: true,
-		},
-		adapter: adapter(),
-	},
-	compilerOptions: {
-		experimental: {
-			async: true,
-		},
-	},
+  preprocess: vitePreprocess(),
+  kit: {
+    alias: {
+      "$/*": "./src/lib/*",
+      "$routes/*": "./src/routes/*",
+    },
+    experimental: {
+      remoteFunctions: true,
+    },
+    adapter: adapter(),
+  },
+  compilerOptions: {
+    experimental: {
+      async: true,
+    },
+  },
 };
 
 export default config;
