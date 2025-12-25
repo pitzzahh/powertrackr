c9074b8350db0961230660a649ab4efe85cb1bcd
pitzzahh <pitzzahh@gmail.com>
2025-12-25 23:25:02 +0800

    chore: export DataTable and DataTablePagination

--- src/lib/components/data-table/index.ts
+++ src/lib/components/data-table/index.ts
@@ -1,3 +1,5 @@
 export { default as DataTableViewOptions } from "./data-table-view-options.svelte";
 export { default as DataTableSearchFilter } from "./data-table-search-filter.svelte";
 export { default as DataTableFacetedFilter } from "./data-table-faceted-filter.svelte";
+export { default as DataTablePagination } from "./data-table-pagination.svelte";
+export { default as DataTable } from "./data-table.svelte";
