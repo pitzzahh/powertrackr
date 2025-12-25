b4f077af025366a0ca4c80d03c2a314146da857c
pitzzahh <pitzzahh@gmail.com>
2025-12-25 23:24:06 +0800

    feat: add New Bill quick action to header

--- src/routes/(components)/header.svelte
+++ src/routes/(components)/header.svelte
@@ -2,13 +2,22 @@
   import Logo from "$/components/logo.svelte";
   import { Button, buttonVariants } from "$/components/ui/button";
   import * as Sheet from "$lib/components/ui/sheet/index.js";
-  import { Menu } from "@lucide/svelte";
+  import { Menu, PhilippinePeso } from "$/assets/icons";
   import SidebarContent from "$routes/(components)/sidebar-content.svelte";
   import { cn } from "$/utils/style";
   import { MoonIcon, SunIcon } from "@lucide/svelte";
   import { toggleMode } from "mode-watcher";
-  let { open } = $state({
+  let { open, quickActions } = $state({
     open: false,
+    quickActions: [
+      {
+        icon: PhilippinePeso,
+        content: "New Bill",
+        action: () => {
+          // TODO: Implement new bill action, e.g., open a modal
+        },
+      },
+    ],
   });
 </script>
 
@@ -28,15 +37,25 @@
         <span class="sr-only">Open sidebar</span>
       </Sheet.Trigger>
       {@render logo({ className: "mx-auto w-1/2 md:w-fit md:m-0 md:pl-0!" })}
-      <Button onclick={toggleMode} variant="secondary" size="icon">
-        <SunIcon
-          class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
-        />
-        <MoonIcon
-          class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
-        />
-        <span class="sr-only">Toggle theme</span>
-      </Button>
+
+      <div class="flex items-center justify-center gap-4">
+        <div class="items-center justify-center gap-2 hidden md:flex">
+          {#each quickActions as quickAction (quickAction.content)}
+            {@const Icon = quickAction.icon}
+            <Button><Icon /> {quickAction.content}</Button>
+          {/each}
+        </div>
+
+        <Button onclick={toggleMode} variant="secondary" size="icon">
+          <SunIcon
+            class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
+          />
+          <MoonIcon
+            class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
+          />
+          <span class="sr-only">Toggle theme</span>
+        </Button>
+      </div>
     </div>
   </header>
 
