fde9122ac018f62cdca528f356cc4948bd36de64
pitzzahh <pitzzahh@gmail.com>
2025-12-25 14:42:50 +0800

    refactor: move nav to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
   import { ModeWatcher } from "mode-watcher";
   import favicon from "$lib/assets/favicon.svg";
   import FocusRing from "$lib/components/focus-ring.svelte";
+  import Header from "$routes/(components)/header.svelte";
+  import SidebarContent from "$routes/(components)/sidebar-content.svelte";
 
   const { children } = $props();
 </script>
@@ -13,4 +15,20 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
-{@render children()}
+
+<div class="relative h-screen w-full overflow-hidden">
+  <Header />
+
+  <div class="h-full overflow-y-auto no-scrollbar">
+    <main class="flex justify-between gap-6 p-4 pt-24 min-h-full">
+      <aside
+        class="sticky rounded-md top-24 h-[calc(100vh-8rem)] md:w-48 lg:w-54 bg-muted hidden md:flex flex-col p-4 overflow-y-auto"
+      >
+        <SidebarContent />
+      </aside>
+      <div class="flex-1 flex flex-col gap-6 min-w-0">
+        {@render children()}
+      </div>
+    </main>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
   import { Wallet } from "@lucide/svelte";
-  import Header from "$routes/(components)/header.svelte";
-  import SidebarContent from "$routes/(components)/sidebar-content.svelte";
   import { formatNumber } from "$/utils/format";
   import ChartArea from "$routes/(components)/chart-area.svelte";
 </script>
 
-<div class="relative h-screen w-full overflow-hidden">
-  <Header />
-
-  <div class="h-full overflow-y-auto no-scrollbar">
-    <main class="flex justify-between gap-6 p-4 pt-24 min-h-full">
-      <aside
-        class="sticky rounded-md top-24 h-[calc(100vh-8rem)] md:w-48 lg:w-54 bg-muted hidden md:flex flex-col p-4 overflow-y-auto"
-      >
-        <SidebarContent />
-      </aside>
-      <div class="flex-1 flex flex-col gap-6 min-w-0">
-        {@render Metrics()}
-        <ChartArea />
-        <!-- <DashboardMetrics />
-        <PerformanceChart />
-        <TickerList /> -->
-      </div>
-    </main>
-  </div>
-</div>
+{@render Metrics()}
+<ChartArea />
 
 {#snippet Metrics()}
   <div
