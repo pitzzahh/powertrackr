a8560943f3319e1ec3b362e0cad29e6493e6ec76
pitzzahh <pitzzahh@gmail.com>
2025-12-25 19:58:49 +0800

    feat: add one file icon resource

---
+++ src/lib/assets/icons.ts
@@ -0,0 +1,4 @@
+export { default as ChevronRight } from "@lucide/svelte/icons/chevron-right";
+export { default as ChevronLeft } from "@lucide/icons/icons/chevron-left";
+export { default as ChevronDown } from "@lucide/svelte/icons/chevron-down";
+export { default as TwoColumns } from "@lucide/svelte/icons/columns-2";
