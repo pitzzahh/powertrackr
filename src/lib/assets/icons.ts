c45d00433779064215a4ed56143c2d39653d6a89
pitzzahh <pitzzahh@gmail.com>
2025-12-25 20:20:58 +0800

    refactor: add more icons

--- src/lib/assets/icons.ts
+++ src/lib/assets/icons.ts
@@ -2,3 +2,6 @@ export { default as ChevronRight } from "@lucide/svelte/icons/chevron-right";
 export { default as ChevronLeft } from "@lucide/icons/icons/chevron-left";
 export { default as ChevronDown } from "@lucide/svelte/icons/chevron-down";
 export { default as TwoColumns } from "@lucide/svelte/icons/columns-2";
+export { default as Settings } from "@lucide/svelte/icons/settings";
+export { default as CirclePlus } from "@lucide/svelte/icons/circle-plus";
+export { default as Check } from "@lucide/svelte/icons/check";
