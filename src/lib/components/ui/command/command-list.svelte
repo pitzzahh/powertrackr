aa104a844f40ce6d3cc9585f45b3464d777c62bb
pitzzahh <pitzzahh@gmail.com>
2025-12-25 20:15:48 +0800

    feat: add command component

---
+++ src/lib/components/ui/command/command-list.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Command as CommandPrimitive } from "bits-ui";
+  import { cn } from "$lib/utils/style.js";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    ...restProps
+  }: CommandPrimitive.ListProps = $props();
+</script>
+
+<CommandPrimitive.List
+  bind:ref
+  data-slot="command-list"
+  class={cn(
+    "max-h-75 scroll-py-1 overflow-x-hidden overflow-y-auto",
+    className,
+  )}
+  {...restProps}
+/>
---
+++ src/lib/components/ui/command/command-shortcut.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { WithElementRef } from "$/index";
+  import { cn } from "$lib/utils/style.js";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
+</script>
+
+<span
+  bind:this={ref}
+  data-slot="command-shortcut"
+  class={cn("text-muted-foreground ms-auto text-xs tracking-widest", className)}
+  {...restProps}
+>
+  {@render children?.()}
+</span>
