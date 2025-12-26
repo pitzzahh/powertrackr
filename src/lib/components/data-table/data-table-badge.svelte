d95e96deb3b7b4bfcf5a890d087e2b5157a44472
pitzzahh <pitzzahh@gmail.com>
2025-12-26 16:13:25 +0800

    feat: add new reusable components for data table

---
+++ src/lib/components/data-table/data-table-badge.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Badge } from "$/components/ui/badge";
+  import type { BadgeVariant } from "$/components/ui/badge";
+  import { cn } from "$/utils/style";
+
+  interface Props {
+    value: boolean | string | number | null;
+    variant?: BadgeVariant;
+    className?: string;
+  }
+
+  let { value, className, variant = "default" }: Props = $props();
+</script>
+
+<Badge {variant} class={cn("text-ellipsis whitespace-nowrap", className)}
+  >{value}</Badge
+>
