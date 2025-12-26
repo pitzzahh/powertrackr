d68ccb5b6ec750ffc14857082710e93a589acb9e
pitzzahh <pitzzahh@gmail.com>
2025-12-26 21:18:36 +0800

    feat: add data-table cell, checkbox and column header

---
+++ src/lib/components/data-table/data-table-checkbox.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type {
+    Checkbox as CheckboxPrimitive,
+    WithoutChildrenOrChild,
+  } from "bits-ui";
+  import { Checkbox } from "$/components/ui/checkbox/index.js";
+
+  let {
+    checked = false,
+    onCheckedChange = (v) => (checked = v),
+    ...restProps
+  }: WithoutChildrenOrChild<CheckboxPrimitive.RootProps> = $props();
+</script>
+
+<Checkbox bind:checked={() => checked, onCheckedChange} {...restProps} />
