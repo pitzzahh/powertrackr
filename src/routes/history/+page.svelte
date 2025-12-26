6426800aae6467d55e74245bf10b9433dcbcdd57
pitzzahh <pitzzahh@gmail.com>
2025-12-27 00:38:43 +0800

    feat: render HistoryDataTable with billing data

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,1 +1,10 @@
-History page
+<script>
+  import { getBillingInfos } from "$/remotes/billing-info.remote";
+  import { HistoryDataTable } from "$routes/history/(components)";
+</script>
+
+<HistoryDataTable
+  data={await getBillingInfos({
+    userId: "5wqtwauhbzkfcqo",
+  })}
+/>
