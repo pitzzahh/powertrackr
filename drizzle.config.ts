a0d8f54912f5a3be5d50a72cae9c12bf5c0808ab
pitzzahh <pitzzahh@gmail.com>
2025-12-26 21:56:11 +0800

    refactor: require DATABASE_URL and set dialect to SQLite

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
+}
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/lib/server/db/schema/index.ts",
-  dialect: "postgresql",
+  dialect: "sqlite",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 });
