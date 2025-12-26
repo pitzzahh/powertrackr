3a427e038f7a511deac5dc7c4c076cf89b9fe4d5
pitzzahh <pitzzahh@gmail.com>
2025-12-26 21:10:12 +0800

    feat: add formatDate utility and typings

--- src/lib/utils/format.ts
+++ src/lib/utils/format.ts
@@ -4,6 +4,29 @@ export const formatNumber = (
 ): string => {
   const { currency = "PHP", locale = "en-PH" } = options;
   const locales = [locale];
-  const intlOptions = { style: "currency", currency };
+  const intlOptions: Intl.NumberFormatOptions = { style: "currency", currency };
   return new Intl.NumberFormat(locales, intlOptions).format(num);
+};
+
+export enum DateFormat {
+  DateOnly = "dateOnly",
+  DateTime = "dateTime",
+}
+
+export const formatDate = (
+  date: Date,
+  options: { format?: DateFormat; locale?: string } = {},
+): string => {
+  const { format = DateFormat.DateOnly, locale = "en-PH" } = options;
+  const intlOptions: Intl.DateTimeFormatOptions =
+    format === DateFormat.DateOnly
+      ? { year: "numeric", month: "long", day: "numeric" }
+      : {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "2-digit",
+        };
+  return new Intl.DateTimeFormat(locale, intlOptions).format(date);
 };
