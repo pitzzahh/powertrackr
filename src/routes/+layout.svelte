54667a3ac10ae6aa4ec038051e33fc35054fae2d
pitzzahh <pitzzahh@gmail.com>
2025-12-27 01:02:48 +0800

    chore: remove logged in state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,8 +8,6 @@
   import { scale } from "svelte/transition";
 
   const { children } = $props();
-
-  let loggedIn = $state(true); // TODO: Replace with actual auth logic
 </script>
 
 <ModeWatcher />
@@ -19,24 +17,20 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{#if loggedIn}
-  <div class="relative h-screen w-full overflow-hidden">
-    <Header />
+<div class="relative h-screen w-full overflow-hidden">
+  <Header />
 
-    <div class="h-full overflow-y-auto no-scrollbar">
-      <main class="flex justify-between gap-4 p-4 pt-16 min-h-full">
-        <aside
-          in:scale={{ duration: 150 }}
-          class="sticky rounded-md top-16 h-[calc(100vh-5rem)] md:w-48 lg:w-54 bg-muted hidden md:flex flex-col p-4 overflow-y-auto"
-        >
-          <SidebarContent />
-        </aside>
-        <div class="flex-1 flex flex-col gap-4 min-w-0 p-1">
-          {@render children()}
-        </div>
-      </main>
-    </div>
+  <div class="h-full overflow-y-auto no-scrollbar">
+    <main class="flex justify-between gap-4 p-4 pt-16 min-h-full">
+      <aside
+        in:scale={{ duration: 150 }}
+        class="sticky rounded-md top-16 h-[calc(100vh-5rem)] md:w-48 lg:w-54 bg-muted hidden md:flex flex-col p-4 overflow-y-auto"
+      >
+        <SidebarContent open={false} />
+      </aside>
+      <div class="flex-1 flex flex-col gap-4 min-w-0 p-1">
+        {@render children()}
+      </div>
+    </main>
   </div>
-{:else}
-  {@render children()}
-{/if}
+</div>
