e32cdd9d8b5a76083c194a1e96e9ab4b59769702
pitzzahh <pitzzahh@gmail.com>
2025-12-27 20:35:42 +0800

    style: make the sidebar trigger visible on medium viewports by shifting its hide breakpoint to lg. Also apply code formatting to the header file.

--- src/routes/(components)/header.svelte
+++ src/routes/(components)/header.svelte
@@ -1,81 +1,83 @@
 <script lang="ts">
-  import Logo from "$/components/logo.svelte";
-  import { Button, buttonVariants } from "$/components/ui/button";
-  import * as Sheet from "$lib/components/ui/sheet/index.js";
-  import { Menu, PhilippinePeso } from "$/assets/icons";
-  import SidebarContent from "$routes/(components)/sidebar-content.svelte";
-  import { cn } from "$/utils/style";
-  import { MoonIcon, SunIcon } from "@lucide/svelte";
-  import { toggleMode } from "mode-watcher";
-  import { scale } from "svelte/transition";
-  import { cubicInOut } from "svelte/easing";
-  let { open, quickActions } = $state({
-    open: false,
-    quickActions: [
-      {
-        icon: PhilippinePeso,
-        content: "New Bill",
-        action: () => {
-          // TODO: Implement new bill action, e.g., open a modal
-        },
-      },
-    ],
-  });
+    import Logo from "$/components/logo.svelte";
+    import { Button, buttonVariants } from "$/components/ui/button";
+    import * as Sheet from "$lib/components/ui/sheet/index.js";
+    import { Menu, PhilippinePeso } from "$/assets/icons";
+    import SidebarContent from "$routes/(components)/sidebar-content.svelte";
+    import { cn } from "$/utils/style";
+    import { MoonIcon, SunIcon } from "@lucide/svelte";
+    import { toggleMode } from "mode-watcher";
+    import { scale } from "svelte/transition";
+    import { cubicInOut } from "svelte/easing";
+    let { open, quickActions } = $state({
+        open: false,
+        quickActions: [
+            {
+                icon: PhilippinePeso,
+                content: "New Bill",
+                action: () => {
+                    // TODO: Implement new bill action, e.g., open a modal
+                },
+            },
+        ],
+    });
 </script>
 
 <Sheet.Root bind:open>
-  <header
-    class="absolute top-0 left-0 right-0 z-50 flex items-center justify-between p-4 bg-background/10 backdrop-blur-[120px]"
-  >
-    <div class="flex items-center gap-4 w-full md:justify-between">
-      <Sheet.Trigger
-        class={buttonVariants({
-          variant: "outline",
-          size: "icon",
-          className: "md:hidden",
-        })}
-      >
-        <Menu class="h-4 w-4" />
-        <span class="sr-only">Open sidebar</span>
-      </Sheet.Trigger>
-      {@render logo({ className: "mx-auto w-1/2 md:w-fit md:m-0 md:pl-0!" })}
-
-      <div class="flex items-center justify-center gap-4">
-        <div class="items-center justify-center gap-2 hidden md:flex">
-          {#each quickActions as quickAction, index (quickAction.content)}
-            {@const Icon = quickAction.icon}
-            <span
-              in:scale={{
-                duration: 250,
-                delay: index * 150,
-                easing: cubicInOut,
-                start: 0.8,
-              }}
+    <header
+        class="absolute top-0 left-0 right-0 z-50 flex items-center justify-between p-4 bg-background/10 backdrop-blur-[120px]"
+    >
+        <div class="flex items-center gap-4 w-full md:justify-between">
+            <Sheet.Trigger
+                class={buttonVariants({
+                    variant: "outline",
+                    size: "icon",
+                    className: "lg:hidden",
+                })}
             >
-              <Button><Icon /> {quickAction.content}</Button>
-            </span>
-          {/each}
+                <Menu class="h-4 w-4" />
+                <span class="sr-only">Open sidebar</span>
+            </Sheet.Trigger>
+            {@render logo({
+                className: "mx-auto w-1/2 md:w-fit md:m-0 md:pl-0!",
+            })}
+
+            <div class="flex items-center justify-center gap-4">
+                <div class="items-center justify-center gap-2 hidden md:flex">
+                    {#each quickActions as quickAction, index (quickAction.content)}
+                        {@const Icon = quickAction.icon}
+                        <span
+                            in:scale={{
+                                duration: 250,
+                                delay: index * 150,
+                                easing: cubicInOut,
+                                start: 0.8,
+                            }}
+                        >
+                            <Button><Icon /> {quickAction.content}</Button>
+                        </span>
+                    {/each}
+                </div>
+
+                <Button onclick={toggleMode} variant="secondary" size="icon">
+                    <SunIcon
+                        class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
+                    />
+                    <MoonIcon
+                        class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
+                    />
+                    <span class="sr-only">Toggle theme</span>
+                </Button>
+            </div>
         </div>
+    </header>
 
-        <Button onclick={toggleMode} variant="secondary" size="icon">
-          <SunIcon
-            class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
-          />
-          <MoonIcon
-            class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
-          />
-          <span class="sr-only">Toggle theme</span>
-        </Button>
-      </div>
-    </div>
-  </header>
-
-  <Sheet.Content side="left" class="bg-muted p-4 flex flex-col h-full w-full">
-    {@render logo({ className: "py-6 w-fit mx-auto" })}
-    <SidebarContent bind:open />
-  </Sheet.Content>
+    <Sheet.Content side="left" class="bg-muted p-4 flex flex-col h-full w-full">
+        {@render logo({ className: "py-6 w-fit mx-auto" })}
+        <SidebarContent bind:open />
+    </Sheet.Content>
 </Sheet.Root>
 
 {#snippet logo({ className }: { className?: string } | undefined = {})}
-  <Logo variant="ghost" class={cn("px-0", className)} />
+    <Logo variant="ghost" class={cn("px-0", className)} />
 {/snippet}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,36 @@
 <script lang="ts">
-  import "./layout.css";
-  import { ModeWatcher } from "mode-watcher";
-  import favicon from "$lib/assets/favicon.svg";
-  import FocusRing from "$lib/components/focus-ring.svelte";
-  import Header from "$routes/(components)/header.svelte";
-  import SidebarContent from "$routes/(components)/sidebar-content.svelte";
-  import { scale } from "svelte/transition";
+    import "./layout.css";
+    import { ModeWatcher } from "mode-watcher";
+    import favicon from "$lib/assets/favicon.svg";
+    import FocusRing from "$lib/components/focus-ring.svelte";
+    import Header from "$routes/(components)/header.svelte";
+    import SidebarContent from "$routes/(components)/sidebar-content.svelte";
+    import { scale } from "svelte/transition";
 
-  const { children } = $props();
+    const { children } = $props();
 </script>
 
 <ModeWatcher />
 <FocusRing />
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
 </svelte:head>
 
 <div class="relative h-screen w-full overflow-hidden">
-  <Header />
+    <Header />
 
-  <div class="h-full overflow-y-auto no-scrollbar">
-    <main class="flex justify-between gap-4 p-4 pt-16 min-h-full">
-      <aside
-        in:scale={{ duration: 150 }}
-        class="sticky rounded-md top-16 h-[calc(100vh-5rem)] md:w-48 lg:w-54 bg-muted hidden md:flex flex-col p-4 overflow-y-auto"
-      >
-        <SidebarContent open={false} />
-      </aside>
-      <div class="flex-1 flex flex-col gap-4 min-w-0 p-1">
-        {@render children()}
-      </div>
-    </main>
-  </div>
+    <div class="h-full overflow-y-auto no-scrollbar">
+        <main class="flex justify-between gap-4 p-4 pt-16 min-h-full">
+            <aside
+                in:scale={{ duration: 150 }}
+                class="sticky rounded-md top-16 h-[calc(100vh-5rem)] md:w-48 lg:w-54 bg-muted hidden lg:flex flex-col p-4 overflow-y-auto"
+            >
+                <SidebarContent open={false} />
+            </aside>
+            <div class="flex-1 flex flex-col gap-4 min-w-0 p-1">
+                {@render children()}
+            </div>
+        </main>
+    </div>
 </div>
