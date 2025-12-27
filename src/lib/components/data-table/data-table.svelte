88a3a2e3b0ed6291ed4ea19724389a7b5a73c704
pitzzahh <pitzzahh@gmail.com>
2025-12-27 16:18:18 +0800

    refactor: set default pageSize to 10 and add rowCount getter

--- src/lib/components/data-table/data-table.svelte
+++ src/lib/components/data-table/data-table.svelte
@@ -63,7 +63,7 @@
             columnVisibility: {},
             columnFilters: [],
             sorting: [],
-            pagination: { pageIndex: 0, pageSize: custom_row_count },
+            pagination: { pageIndex: 0, pageSize: 10 },
         });
 
     const table = createSvelteTable({
@@ -89,6 +89,9 @@
         },
         get columns() {
             return columns;
+        },
+        get rowCount() {
+            return custom_row_count;
         },
         enableRowSelection: true,
         onRowSelectionChange: (updater) => {
