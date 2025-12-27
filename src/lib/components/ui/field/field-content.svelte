bbcfe407de30892edb32d5bed84d95449991af7d
pitzzahh <pitzzahh@gmail.com>
2025-12-27 10:24:39 +0800

    feat: add field component

---
+++ src/lib/components/ui/field/field-content.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { WithElementRef } from "$/index";
+  import { cn } from "$lib/utils/style.js";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div
+  bind:this={ref}
+  data-slot="field-content"
+  class={cn(
+    "group/field-content flex flex-1 flex-col gap-1.5 leading-snug",
+    className,
+  )}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
