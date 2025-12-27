5d0bbf7439c503120b0fe58ae27258ac1f8a589c
pitzzahh <pitzzahh@gmail.com>
2025-12-27 10:27:55 +0800

    feat: add calendar component

---
+++ src/lib/components/ui/calendar/calendar-months.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+  import { cn } from "$lib/utils/style.js";
+  import type { WithElementRef } from "$/index";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div
+  bind:this={ref}
+  class={cn("relative flex flex-col gap-4 md:flex-row", className)}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
