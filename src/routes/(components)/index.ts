0f04a29f68c31eb15c2c715f2cb51cfe06a47cf7
pitzzahh <pitzzahh@gmail.com>
2025-12-27 19:43:15 +0800

    feat: add TIME_RANGE_OPTIONS constant

---
+++ src/routes/(components)/index.ts
@@ -0,0 +1,8 @@
+export const TIME_RANGE_OPTIONS = [
+  { value: "7d", label: "Last 7 days" },
+  { value: "30d", label: "Last 30 days" },
+  { value: "90d", label: "Last 3 months" },
+  { value: "6m", label: "Last 6 months" },
+  { value: "1y", label: "Last year" },
+  { value: "all", label: "Show All" },
+];
