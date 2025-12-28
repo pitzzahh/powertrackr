8eaee321fd3c60047d1426e85cfb976dcbef8873
pitzzahh <pitzzahh@gmail.com>
2025-12-28 20:09:02 +0800

    remove: relation exports from schema index

--- src/lib/server/db/schema/index.ts
+++ src/lib/server/db/schema/index.ts
@@ -4,10 +4,3 @@ export { payment } from "./payment";
 export { billingInfo } from "./billing-info";
 export { key } from "./key";
 export { session } from "./session";
-
-// Export all relations
-export { userRelations } from "../relations/user";
-export { paymentRelations } from "../relations/payment";
-export { billingInfoRelations } from "../relations/billing-info";
-export { keyRelations } from "../relations/key";
-export { sessionRelations } from "../relations/session";
