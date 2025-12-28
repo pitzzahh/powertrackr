ced2080b52e81a7977da38bc6fb0c1ddab73f43c
pitzzahh <pitzzahh@gmail.com>
2025-12-28 22:16:37 +0800

    refactor: improve validation messages for billing schema

--- src/lib/schemas/billing-info.ts
+++ src/lib/schemas/billing-info.ts
@@ -1,12 +1,16 @@
 import { z } from "zod";
 
 export const billFormSchema = z.object({
-  date: z.string().refine((v) => v, { message: "Date is required" }),
-  balance: z.number().gt(0, { message: "Balance must be greater than 0" }),
-  totalKwh: z.number().gt(0, { message: "Total Kwh must be greater than 0" }),
+  date: z.string().refine((v) => v, { message: "is required" }),
+  balance: z
+    .number({ message: "must be a number" })
+    .gt(0, { message: "must be greater than 0" }),
+  totalKwh: z
+    .number({ message: "must be a number" })
+    .gt(0, { message: "must be greater than 0" }),
   subReading: z
-    .number()
-    .gt(0, { message: "Sub Reading must be greater than 0" })
+    .number({ message: "must be a number" })
+    .gt(0, { message: "must be greater than 0" })
     .optional(),
   status: z.enum(["Paid", "Pending"]),
 });
