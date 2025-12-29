fc37a89dc1bc77b59f2cfce41c34914eb30356d3
pitzzahh <pitzzahh@gmail.com>
2025-12-29 14:37:44 +0800

    refactor: hydrate billing infos on mount

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -2,16 +2,18 @@
     import { getBillingInfos } from "$/remotes/billing-info.remote";
     import type { BillingInfo } from "$/types/billing-info";
     import { HistoryDataTable } from "$routes/history/(components)";
-    import { onMount } from "svelte";
+    import { hydratable, onMount } from "svelte";
 
     let { billingInfos }: { billingInfos: BillingInfo[] } = $state({
         billingInfos: [],
     });
 
     onMount(async () => {
-        billingInfos = await getBillingInfos({
-            userId: "5wqtwauhbzkfcqo",
-        });
+        billingInfos = await hydratable("history_data", () =>
+            getBillingInfos({
+                userId: "5wqtwauhbzkfcqo",
+            }),
+        );
     });
 </script>
 
