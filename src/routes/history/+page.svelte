32b61b6933f43defd8e5e486d9715be55e705d6a
pitzzahh <pitzzahh@gmail.com>
2025-12-29 10:49:37 +0800

    refactor: fetch billing data in onMount and add types

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,10 +1,18 @@
-<script>
-  import { getBillingInfos } from "$/remotes/billing-info.remote";
-  import { HistoryDataTable } from "$routes/history/(components)";
+<script lang="ts">
+    import { getBillingInfos } from "$/remotes/billing-info.remote";
+    import type { BillingInfo } from "$/types/billing-info";
+    import { HistoryDataTable } from "$routes/history/(components)";
+    import { onMount } from "svelte";
+
+    let { billingInfos }: { billingInfos: BillingInfo[] } = $state({
+        billingInfos: [],
+    });
+
+    onMount(async () => {
+        billingInfos = await getBillingInfos({
+            userId: "5wqtwauhbzkfcqo",
+        });
+    });
 </script>
 
-<HistoryDataTable
-  data={await getBillingInfos({
-    userId: "5wqtwauhbzkfcqo",
-  })}
-/>
+<HistoryDataTable data={billingInfos} />
