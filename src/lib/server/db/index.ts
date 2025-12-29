f44f981b40325ce2ea1f7d285e76e1959f6d65e1
pitzzahh <pitzzahh@gmail.com>
2025-12-29 09:39:05 +0800

    refactor: initialize Drizzle with connection URL instead of its driver

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,8 +1,7 @@
-import { Database } from "@sqlitecloud/drivers";
 import { drizzle } from "drizzle-orm/sqlite-cloud";
 import { DATABASE_URL } from "$env/static/private";
 import { relations } from "./relations";
 
-const db = drizzle({ client: new Database(DATABASE_URL), relations });
+const db = drizzle({ connection: DATABASE_URL, relations });
 
 export { db };
