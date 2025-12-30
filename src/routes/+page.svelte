67f4f575e7ba4a27cbc697fb16029a647d234918
pitzzahh <pitzzahh@gmail.com>
2025-12-30 22:23:34 +0800

    refactor: show billing summary with loaders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { Wallet } from "@lucide/svelte";
     import { formatNumber } from "$/utils/format";
     import {
         ChartArea,
@@ -9,8 +8,12 @@
     } from "$routes/(components)";
     import { scale } from "svelte/transition";
     import { cubicInOut } from "svelte/easing";
-    import { getExtendedBillingInfos } from "$lib/remotes/billing-info.remote";
+    import {
+        getExtendedBillingInfos,
+        getBillingSummary,
+    } from "$lib/remotes/billing-info.remote";
     import { hydratable, onMount } from "svelte";
+    import { Loader, Banknote } from "$lib/assets/icons";
     import type { ExtendedBillingInfo } from "$/types/billing-info";
 
     let {
@@ -23,6 +26,8 @@
         extendedBillingInfos: [],
         status: "fetching",
     });
+
+    const summary = $derived(getBillingSummary({ userId: "5wqtwauhbzkfcqo" }));
 
     onMount(async () => {
         status = "fetching";
@@ -57,41 +62,88 @@
     >
         <div class="flex flex-col gap-2">
             <div class="flex items-center gap-2">
-                <Wallet class="h-5 w-5" />
+                <Banknote class="h-5 w-5" />
                 <span class="text-lg">Current</span>
             </div>
-            <div class="text-5xl md:text-4xl lg:text-5xl font-bold">
-                {formatNumber(6810)}
-            </div>
+            {#if summary?.loading}
+                <Loader class="h-5 w-5 animate-spin" />
+            {:else if summary?.error}
+                <div class="text-5xl md:text-4xl lg:text-5xl font-bold">0</div>
+            {:else}
+                <div class="text-5xl md:text-4xl lg:text-5xl font-bold">
+                    {formatNumber(summary.current?.current || 0)}
+                </div>
+            {/if}
         </div>
 
         <div class="grid grid-cols-2 md:grid-cols-4 gap-8 xl:gap-16">
             <div class="flex flex-col gap-1">
                 <span class="text-sm">Invested</span>
-                <span class="text-2xl md:text-xl lg:text-2xl font-semibold"
-                    >$5,220</span
-                >
+                {#if summary?.loading}
+                    <Loader class="h-4 w-4 animate-spin" />
+                {:else if summary?.error}
+                    <span class="text-2xl md:text-xl lg:text-2xl font-semibold"
+                        >0</span
+                    >
+                {:else}
+                    <span class="text-2xl md:text-xl lg:text-2xl font-semibold"
+                        >{formatNumber(summary.current?.invested || 0)}</span
+                    >
+                {/if}
             </div>
             <div class="flex flex-col gap-1">
                 <span class="text-sm">Total Returns</span>
-                <span
-                    class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
-                    >+{formatNumber(1590)}</span
-                >
+                {#if summary?.loading}
+                    <Loader class="h-4 w-4 animate-spin" />
+                {:else if summary?.error}
+                    <span
+                        class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
+                        >+0</span
+                    >
+                {:else}
+                    <span
+                        class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
+                        >+{formatNumber(
+                            summary.current?.totalReturns || 0,
+                        )}</span
+                    >
+                {/if}
             </div>
             <div class="flex flex-col gap-1">
                 <span class="text-sm">Net Returns</span>
-                <span
-                    class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
-                    >+30.46%</span
-                >
+                {#if summary?.loading}
+                    <Loader class="h-4 w-4 animate-spin" />
+                {:else if summary?.error}
+                    <span
+                        class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
+                        >+0%</span
+                    >
+                {:else}
+                    <span
+                        class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
+                        >+{formatNumber(
+                            summary.current?.netReturns || 0,
+                        )}%</span
+                    >
+                {/if}
             </div>
             <div class="flex flex-col gap-1">
                 <span class="text-sm">1 Day Returns</span>
-                <span
-                    class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
-                    >+{formatNumber(142.5)}</span
-                >
+                {#if summary?.loading}
+                    <Loader class="h-4 w-4 animate-spin" />
+                {:else if summary?.error}
+                    <span
+                        class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
+                        >+0</span
+                    >
+                {:else}
+                    <span
+                        class="text-2xl md:text-xl lg:text-2xl font-semibold text-green-600"
+                        >+{formatNumber(
+                            summary.current?.oneDayReturns || 0,
+                        )}</span
+                    >
+                {/if}
             </div>
         </div>
     </section>
