b01d9af54e0e1799bd431457870a4c7ddf68776b
pitzzahh <pitzzahh@gmail.com>
2025-12-31 14:48:24 +0800

    chore: remove start script

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,12 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
+import type { Session, User } from "better-auth";
+
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    interface Locals {
+      session?: Session;
+      user?: User;
+    }
+  }
 }
 
 export {};
