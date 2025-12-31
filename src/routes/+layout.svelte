e74a780f75a7cd7bcca306f0f754cc199fce196b
pitzzahh <pitzzahh@gmail.com>
2025-12-31 16:09:54 +0800

    refactor: render full layout only for authenticated users

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-    import "./layout.css";
-    import { ModeWatcher } from "mode-watcher";
-    import favicon from "$/assets/favicon.svg";
-    import FocusRing from "$/components/focus-ring.svelte";
-    import Header from "$routes/(components)/header.svelte";
-    import { Toaster } from "$/components/ui/sonner/index.js";
+  import "./layout.css";
+  import { ModeWatcher } from "mode-watcher";
+  import favicon from "$/assets/favicon.svg";
+  import FocusRing from "$/components/focus-ring.svelte";
+  import Header from "$routes/(components)/header.svelte";
+  import { Toaster } from "$/components/ui/sonner/index.js";
 
-    import SidebarContent from "$routes/(components)/sidebar-content.svelte";
-    import { scale } from "svelte/transition";
+  import SidebarContent from "$routes/(components)/sidebar-content.svelte";
+  import { scale } from "svelte/transition";
 
-    const { children } = $props();
+  const { children, data } = $props();
 </script>
 
 <ModeWatcher />
@@ -17,23 +17,27 @@
 <Toaster />
 
 <svelte:head>
-    <link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="relative h-screen w-full overflow-hidden">
+{#if data.user && data.session}
+  <div class="relative h-screen w-full overflow-hidden">
     <Header />
 
     <div class="h-full overflow-y-auto no-scrollbar">
-        <main class="flex justify-between gap-4 p-4 pt-16 min-h-full">
-            <aside
-                in:scale={{ duration: 150 }}
-                class="sticky rounded-md top-17 h-[calc(100vh-5.5rem)] md:w-48 lg:w-54 bg-card border shadow-sm hidden lg:flex flex-col p-4 overflow-y-auto"
-            >
-                <SidebarContent open={false} />
-            </aside>
-            <div class="flex-1 flex flex-col gap-4 min-w-0 p-1">
-                {@render children()}
-            </div>
-        </main>
+      <main class="flex justify-between gap-4 p-4 pt-16 min-h-full">
+        <aside
+          in:scale={{ duration: 150 }}
+          class="sticky rounded-md top-17 h-[calc(100vh-5.5rem)] md:w-48 lg:w-54 bg-card border shadow-sm hidden lg:flex flex-col p-4 overflow-y-auto"
+        >
+          <SidebarContent open={false} />
+        </aside>
+        <div class="flex-1 flex flex-col gap-4 min-w-0 p-1">
+          {@render children()}
+        </div>
+      </main>
     </div>
-</div>
+  </div>
+{:else}
+  {@render children()}
+{/if}
