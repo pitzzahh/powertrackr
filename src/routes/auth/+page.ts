9c31728b2ac96500c552c6d72584998a6674adaa
pitzzahh <pitzzahh@gmail.com>
2025-12-31 20:42:52 +0800

    fix: remove throw when redirecting in auth load

--- src/routes/auth/+page.ts
+++ src/routes/auth/+page.ts
@@ -2,10 +2,9 @@ import { redirect } from "@sveltejs/kit";
 
 export function load({ url: { searchParams } }) {
   const actions = ["login", "register"] as const;
-  const act = searchParams.get("act") ?? "login";
-  if (!actions.includes(act as "login" | "register")) {
-    searchParams.set("act", "login");
-    throw redirect(307, `/auth?${searchParams.toString()}`);
+  const act = searchParams.get("act");
+  if (!act || !actions.includes(act as "login" | "register")) {
+    redirect(307, `/auth?act=login`);
   }
   return {
     action: act as (typeof actions)[number],
