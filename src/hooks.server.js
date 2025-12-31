8b7826abc7cb4833d63a15027830fe3c7d672928
pitzzahh <pitzzahh@gmail.com>
2025-12-31 20:39:49 +0800

    chore: redirect if no useer

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,12 +1,15 @@
 import { svelteKitHandler } from "better-auth/svelte-kit";
 import { building, dev } from "$app/environment";
 import { auth } from "$/server/auth";
+import { redirect } from "@sveltejs/kit";
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
   const session = await auth.api.getSession({
     headers: event.request.headers,
   });
+
+  if (!session) redirect(303, "/auth?act=login");
 
   if (session) {
     event.locals.session = session.session;
--- src/routes/auth/+page.ts
+++ src/routes/auth/+page.ts
@@ -4,9 +4,10 @@ export function load({ url: { searchParams } }) {
   const actions = ["login", "register"] as const;
   const act = searchParams.get("act") ?? "login";
   if (!actions.includes(act as "login" | "register")) {
-    redirect(307, "/auth?act=login");
+    searchParams.set("act", "login");
+    throw redirect(307, `/auth?${searchParams.toString()}`);
   }
   return {
-    action: act,
+    action: act as (typeof actions)[number],
   };
 }
