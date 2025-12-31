173fa2b28341d3238da4adbcf23a8088a7fa14ef
pitzzahh <pitzzahh@gmail.com>
2025-12-31 16:59:06 +0800

    feat: export account and verification from schema index

--- src/lib/server/db/schema/index.ts
+++ src/lib/server/db/schema/index.ts
@@ -5,3 +5,5 @@ export { user } from "./user";
 export { payment } from "./payment";
 export { billingInfo } from "./billing-info";
 export { session } from "./session";
+export { account } from "./account";
+export { verification } from "./verification";
