9ee5ed5d195f750df9e4121cc4d08a80fa0189c4
pitzzahh <pitzzahh@gmail.com>
2025-12-31 15:06:13 +0800

    refactor: use better-auth svelteKit handler and set session

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,18 @@
-import { dev } from "$app/environment";
+import { svelteKitHandler } from "better-auth/svelte-kit";
+import { building, dev } from "$app/environment";
+import { auth } from "$/server/auth";
 
-export function handle({ event, resolve }) {
+/** @type {import('@sveltejs/kit').Handle} */
+export const handle = async ({ event, resolve }) => {
+  const session = await auth.api.getSession({
+    headers: event.request.headers,
+  });
+
+  if (session) {
+    event.locals.session = session.session;
+    event.locals.user = session.user;
+  }
+
   if (
     dev &&
     event.url.pathname === "/.well-known/appspecific/com.chrome.devtools.json"
@@ -8,5 +20,5 @@ export function handle({ event, resolve }) {
     return new Response(undefined, { status: 404 });
   }
 
-  return resolve(event);
-}
+  return svelteKitHandler({ event, resolve, auth, building });
+};
