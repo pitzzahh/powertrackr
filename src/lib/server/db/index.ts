b122c5533e7486dcfdaa4ed69ca5e9bf9632a49f
pitzzahh <pitzzahh@gmail.com>
2025-12-31 16:59:22 +0800

    refactor: pass schema to Drizzle DB client

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,7 +2,8 @@ import { drizzle } from "drizzle-orm/sqlite-cloud";
 import { DATABASE_URL } from "$env/static/private";
 import { relations } from "./relations";
 import { Database } from "@sqlitecloud/drivers";
+import * as schema from "./schema/index.js";
 
-const db = drizzle({ client: new Database(DATABASE_URL), relations });
+const db = drizzle({ client: new Database(DATABASE_URL), schema, relations });
 
 export { db };
