64a82690dc10c5d1d45a9e3d56d55d9aceeca4ae
pitzzahh <pitzzahh@gmail.com>
2026-01-02 21:48:13 +0800

    refactor: user db schema add oauth related columns

--- src/lib/server/db/schema/user.ts
+++ src/lib/server/db/schema/user.ts
@@ -5,11 +5,17 @@ export const user = sqliteTable(
   "user",
   (t) => ({
     id: t.text().primaryKey(),
-    githubId: t.integer().unique(),
+    githubId: t.integer("github_id").unique(),
     name: t.text().notNull(),
     email: t.text().notNull(),
     emailVerified: t
       .integer("email_verified", { mode: "boolean" })
+      .default(false)
+      .notNull(),
+    totpKey: t.blob("totp_key"),
+    recoveryCode: t.blob("recovery_code"),
+    registeredTwoFactor: t
+      .integer("registered_two_factor", { mode: "boolean" })
       .default(false)
       .notNull(),
     image: t.text(),
