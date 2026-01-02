96cebd68220e079a4caeda61dfe9e6de01ce54df
pitzzahh <pitzzahh@gmail.com>
2026-01-03 07:50:33 +0800

    refactor: reset auth form status on destroy

--- src/lib/components/auth-form.svelte
+++ src/lib/components/auth-form.svelte
@@ -24,6 +24,7 @@
   import { cn } from "$/utils/style.js";
   import * as Password from "$/components/password";
   import { Github, Loader } from "$/assets/icons";
+  import { onDestroy } from "svelte";
 
   let {
     action,
@@ -37,6 +38,8 @@
   });
 
   const id = $props.id();
+
+  onDestroy(() => (status = "idle"));
 </script>
 
 <form
