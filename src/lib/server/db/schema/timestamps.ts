7b8ca0b600ad393989381f4dab1ad84fe8554f0e
pitzzahh <pitzzahh@gmail.com>
2026-01-02 23:08:36 +0800

    refactor: temp remove notnull constraint on timestamps

--- src/lib/server/db/schema/timestamps.ts
+++ src/lib/server/db/schema/timestamps.ts
@@ -2,11 +2,10 @@ import { sql } from "drizzle-orm";
 import { integer } from "drizzle-orm/sqlite-core";
 
 export const timestamps = {
-  createdAt: integer("created_at", { mode: "timestamp" })
-    .default(sql`(current_timestamp)`)
-    .notNull(),
-  updatedAt: integer("updated_at", { mode: "timestamp" })
-    .default(sql`(current_timestamp)`)
-    .$onUpdate(() => new Date())
-    .notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`(current_timestamp)`,
+  ),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).default(
+    sql`(current_timestamp)`,
+  ),
 };
