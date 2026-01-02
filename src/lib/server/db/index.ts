e175847e84f0d49e8b23a734bc089b3788e758ee
pitzzahh <pitzzahh@gmail.com>
2026-01-02 21:49:37 +0800

    refactor: db to use libsql

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,8 +1,15 @@
-import { drizzle } from "drizzle-orm/sqlite-cloud";
-import { DATABASE_URL } from "$env/static/private";
+import { drizzle } from "drizzle-orm/libsql";
+import { DATABASE_URL, DATABASE_AUTH_TOKEN } from "$env/static/private";
 import { relations } from "./relations";
 import * as schema from "./schema/index.js";
 
-const db = drizzle({ connection: DATABASE_URL, schema, relations });
+const db = drizzle({
+  connection: {
+    url: DATABASE_URL,
+    authToken: DATABASE_AUTH_TOKEN,
+  },
+  schema,
+  relations,
+});
 
 export { db };
