038bc4cec2524abbb5a0f2d1128695885537ca8e
pitzzahh <pitzzahh@gmail.com>
2026-01-02 15:30:19 +0800

    refactor: replace github constant with createGitHub

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,9 +1,10 @@
 import { GitHub } from "arctic";
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
-import { page } from "$app/state";
 
-export const github = new GitHub(
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET,
-  `${page.url.protocol}//${page.url.host}/login/github/callback`,
-);
+export function createGitHub(url: URL) {
+  return new GitHub(
+    GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET,
+    `${url.protocol}//${url.host}/login/github/callback`,
+  );
+}
