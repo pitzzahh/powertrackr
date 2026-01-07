ec87b64066607ee0f60c804fb7d73810107744bb
pitzzahh <pitzzahh@gmail.com>
2026-01-07 22:53:56 +0800

    refactor: adjust billing-info mapper and add extended view

--- src/lib/utils/mapper/billing-info.ts
+++ src/lib/utils/mapper/billing-info.ts
@@ -1,25 +1,40 @@
-import type { BillingInfo, BillingInfoDTO, BillingInfoTableView } from "$/types/billing-info";
+import type {
+  BillingInfo,
+  BillingInfoDTO,
+  BillingInfoTableView,
+  ExtendedBillingInfo,
+  ExtendedBillingInfoTableView,
+} from "$/types/billing-info";
 import { DateFormat, formatDate } from "$/utils/format";
 
 export function billingInfoToDto(original: BillingInfo): BillingInfoDTO {
   return {
     id: original.id,
     date: new Date(original.date),
-    totalKwh: original.totalKwh,
-    subKwh: original.subKwh ?? 0,
-    payPerKwh: original.payPerKwh,
-    subReadingOld: original.subReadingOld ?? 0,
-    subReadingLatest: original.subReadingLatest || 0,
+    totalkWh: original.totalKWh,
     balance: original.balance,
-    payment: -1,
-    subPayment: -1,
-    status: original.status.toLowerCase() as "pending" | "paid",
-    createdAt: original.createdAt,
-    updatedAt: original.updatedAt,
+    payPerkWh: original.payPerKwh,
+    status: original.status as "Pending" | "Paid",
+    subReadingLatest: 0,
   };
 }
 
 export function billingInfoToTableView(original: BillingInfo): BillingInfoTableView {
+  return {
+    ...original,
+    date: formatDate(new Date(original.date)),
+    createdAt: formatDate(original.createdAt ?? new Date(), {
+      format: DateFormat.DateTime,
+    }),
+    updatedAt: formatDate(original.updatedAt ?? new Date(), {
+      format: DateFormat.DateTime,
+    }),
+  };
+}
+
+export function extendedBillingInfoToTableView(
+  original: ExtendedBillingInfo,
+): ExtendedBillingInfoTableView {
   return {
     ...original,
     date: formatDate(new Date(original.date)),
