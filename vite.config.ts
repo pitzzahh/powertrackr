5ee878f564f1d1fdd6d7917bf61964a775c3485b
pitzzahh <pitzzahh@gmail.com>
2026-01-14 13:23:27 +0800

    feat: add enhanced-img and enable Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { enhancedImages } from "@sveltejs/enhanced-img";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), enhancedImages(), sveltekit()],
   build: {
     chunkSizeWarningLimit: 2000,
   },
