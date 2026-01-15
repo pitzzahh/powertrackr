438be65c72117fa7a0d1df0a5108a929f3262b6e
pitzzahh <pitzzahh@gmail.com>
2026-01-16 02:36:40 +0800

    remove: check for DATABASE_AUTH_TOKEN

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL || !process.env.DATABASE_AUTH_TOKEN) {
-  throw new Error("DATABASE_URL and DATABASE_AUTH_TOKEN is not defined in environment variables");
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
 }
 
 export default defineConfig({
