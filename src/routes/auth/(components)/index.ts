b3c2a8dd6267f9f48e68073a1db5cd688392a524
pitzzahh <pitzzahh@gmail.com>
2026-01-18 00:19:36 +0800

    refactor: export Forgot and Reset Password forms

--- src/routes/auth/(components)/index.ts
+++ src/routes/auth/(components)/index.ts
@@ -1,4 +1,6 @@
 export { default as AuthForm } from "./auth-form.svelte";
+export { default as ForgotPasswordForm } from "./forgot-password-form.svelte";
+export { default as ResetPasswordForm } from "./reset-password-form.svelte";
 export { default as Setup2FAForm } from "./setup-2fa-form.svelte";
 export { default as VerifyEmailForm } from "./verify-email-form.svelte";
 export { default as AuthBackground } from "./auth-background.svelte";
