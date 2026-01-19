38e6c93b41ebbd69973109206ccaf886ad9f83dd
pitzzahh <pitzzahh@gmail.com>
2026-01-19 20:08:26 +0800

    refactor: add separators between sub meter entries and fix table row order and labels

--- src/routes/history/(components)/sub-payments-dialog.svelte
+++ src/routes/history/(components)/sub-payments-dialog.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Separator } from "$/components/ui/separator/index.js";
   import { formatNumber } from "$/utils/format";
   import { Dialog, DialogContent, DialogHeader, DialogTitle } from "$lib/components/ui/dialog";
   import { ScrollArea } from "$lib/components/ui/scroll-area";
@@ -20,23 +21,23 @@
       <DialogTitle>Sub Payments for [{billingInfo.date}]</DialogTitle>
     </DialogHeader>
     <ScrollArea class="max-h-96">
-      {#each billingInfo.subMeters as subMeter}
+      {#each billingInfo.subMeters as subMeter, index}
         <div class="space-y-2">
-          <h3 class="text-md mb-2 font-semibold">Sub Meter {subMeter.id}</h3>
+          <h3 class="text-md mb-2 font-semibold">Sub Meter [{subMeter.label}]</h3>
           <div class="overflow-hidden rounded-md border bg-background">
             <Table>
               <TableBody>
                 <TableRow
                   class="*:border-border hover:bg-transparent [&>:not(:last-child)]:border-r"
                 >
-                  <TableCell class="bg-muted/50 py-2 font-medium">Sub kWh</TableCell>
-                  <TableCell class="py-2">{subMeter.subkWh}kWh</TableCell>
+                  <TableCell class="bg-muted/50 py-2 font-medium">Reading</TableCell>
+                  <TableCell class="py-2">{subMeter.reading}</TableCell>
                 </TableRow>
                 <TableRow
                   class="*:border-border hover:bg-transparent [&>:not(:last-child)]:border-r"
                 >
-                  <TableCell class="bg-muted/50 py-2 font-medium">Latest Reading</TableCell>
-                  <TableCell class="py-2">{subMeter.reading}</TableCell>
+                  <TableCell class="bg-muted/50 py-2 font-medium">Used kWh</TableCell>
+                  <TableCell class="py-2">{subMeter.subkWh} kWh</TableCell>
                 </TableRow>
                 <TableRow
                   class="*:border-border hover:bg-transparent [&>:not(:last-child)]:border-r"
@@ -48,6 +49,9 @@
             </Table>
           </div>
         </div>
+        {#if index % 2 == 0}
+          <Separator class="my-6 w-full" />
+        {/if}
       {/each}
     </ScrollArea>
   </DialogContent>
