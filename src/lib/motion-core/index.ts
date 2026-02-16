8efd96f9ee39b1ba073563404682b3051e95765b
pitzzahh <pitzzahh@gmail.com>
2026-02-16 19:54:27 +0800

    refactor: add Marquee and export to motion-core

--- src/lib/motion-core/index.ts
+++ src/lib/motion-core/index.ts
@@ -1,3 +1,4 @@
 export { default as Magnetic } from "./magnetic/Magnetic.svelte";
+export { default as Marquee } from "./marquee/Marquee.svelte";
 export { default as SplitReveal } from "./split-reveal/SplitReveal.svelte";
 export { default as TextLoop } from "./text-loop/TextLoop.svelte";
