91864a76990fa927d1c7066cb4089a54b0975342
pitzzahh <pitzzahh@gmail.com>
2026-02-26 19:20:54 +0800

    remove: unused imports

--- src/routes/history/(components)/sub-payments-button.svelte
+++ src/routes/history/(components)/sub-payments-button.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
   import { CreditCard } from "$/assets/icons";
-  import Button, {
-    type ButtonProps,
-    type ButtonVariant,
-  } from "$/components/ui/button/button.svelte";
+  import Button, { type ButtonProps } from "$/components/ui/button/button.svelte";
   import SubPaymentsDialog from "./sub-payments-dialog.svelte";
   import type { ExtendedBillingInfoTableView } from "$/types/billing-info";
-  import type { Snippet } from "svelte";
-  import type { HTMLButtonAttributes } from "svelte/elements";
 
   let {
     row,
