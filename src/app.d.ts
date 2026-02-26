501d54521d56f84bfa0df9852540459321b75aa3
pitzzahh <pitzzahh@gmail.com>
2026-02-26 10:26:27 +0800

    feat: add Platform interface to global declarations

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,6 +4,10 @@ declare global {
       user: import("$lib/server/auth").SessionValidationResult["user"];
       session: import("$lib/server/auth").SessionValidationResult["session"];
     }
+    interface Platform {
+      server: Bun.Server;
+      request: Request;
+    }
   }
 }
 
