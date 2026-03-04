d2363b956b6a90b168ccee3987ef90fbeeba414c
pitzzahh <pitzzahh@gmail.com>
2026-03-04 21:04:10 +0800

    feat: replace @sveltejs/adapter-node with @sveltejs/adapter-cloudflare and call adapter() without the previous precompress option. Tidy up config formatting.

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,15 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    alias: {
-      "$/*": "./src/lib/*",
-      "$routes/*": "./src/routes/*",
-    },
-    experimental: {
-      remoteFunctions: true,
-    },
-    adapter: adapter({
-      precompress: true,
-    }),
+    alias: { "$/*": "./src/lib/*", "$routes/*": "./src/routes/*" },
+    experimental: { remoteFunctions: true },
+    adapter: adapter(),
   },
-  compilerOptions: {
-    experimental: {
-      async: true,
-    },
-  },
+  compilerOptions: { experimental: { async: true } },
 };
 
 export default config;
