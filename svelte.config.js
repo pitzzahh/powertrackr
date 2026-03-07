d12c4dcb537997b039c4a49a050f874d35947e3f
pitzzahh <pitzzahh@gmail.com>
2026-03-07 14:28:17 +0800

    chore: switch SvelteKit adapter to Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,8 +13,11 @@ const config = {
       remoteFunctions: true,
     },
     adapter: adapter({
-      precompress: true,
+      fallback: "plaintext",
     }),
+    output: {
+      bundleStrategy: "single",
+    },
   },
   compilerOptions: {
     experimental: {
