4192d4c76d6f0b101eee5fd8ddf33c89fc347d6e
pitzzahh <pitzzahh@gmail.com>
2026-03-08 10:05:01 +0800

    chore: add worker-src to Content Security Policy

--- svelte.config.js
+++ svelte.config.js
@@ -21,6 +21,7 @@ const config = {
           "sha256-uQ+6xeJ5jfvD5SmN5W7ZFR4dF9DbDwscZWrWOLfV+RM=",
           "sha256-94WxU203ItVdYeuHa4UBPQzWANAxvaHV/BgTnRrE/14=",
         ],
+        "worker-src": ["self", "blob:"],
         "style-src": ["self", "unsafe-inline", "https://fonts.googleapis.com"],
         "img-src": ["self", "data:"],
         "font-src": ["self", "https://fonts.gstatic.com"],
